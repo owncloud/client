31730eb7fb998134021ec3e3de5266d8a5f52172
Tim Dewhirst <tim@kdab.com>
2013-03-06 17:30:03 +0000

    * WIP; ignore

--- src/mirall/oauth/oauth.cpp
+++ src/mirall/oauth/oauth.cpp
@@ -3,9 +3,21 @@
 
 // qt
 #include <QNetworkAccessManager>
+#include <QNetworkRequest>
+#include <QNetworkReply>
+#include <QUrl>
+#include <QHostAddress>
 #include <QSharedPointer>
+#include <QTcpServer>
+#include <QTimer>
 
 #include <QDebug>
+
+/// OAUTH constants
+static const QString CLIENT_KEY( "a52c72ab0903261fddc804682105f8" );
+static const QString CLIENT_SECRET( "58459d4b1a0134abc624b6bd5249e2" );
+static const QString AUTH_URL( "https://ec2-23-23-37-57.compute-1.amazonaws.com/oauth2/authorize" );
+static const QString TOKEN_URL( "https://ec2-23-23-37-57.compute-1.amazonaws.com/oauth2/token" );
 
 static void nullDeleter( QNetworkAccessManager* )
 {}
@@ -19,6 +31,7 @@ public:
     OAuthPrivate( OAuth* o )
         : QObject( o ),
           owner( o ),
+          listener( NULL ),
           nam( NULL )
     {
         connect( this, SIGNAL( error( Error ) ), owner, SIGNAL( error( Error ) ) );
@@ -43,10 +56,18 @@ public:
     void authenticate()
     {
         // kick off authentication
+        startListener();
         
     }
     
+    void startListener()
+    {
+        
+    }
+
+
     OAuth* owner;
+    QTcpServer* listener;
 
 signals:
     void error( Error );
