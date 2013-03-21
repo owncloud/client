32b8c8296bc52820a3a3573f30fcd0ce5bc8d7ff
Tim Dewhirst <tim@bugless.co.uk>
2013-03-21 16:49:29 +0000

    * revert commented out setupLogBrowser call

--- src/mirall/application.cpp
+++ src/mirall/application.cpp
@@ -100,7 +100,7 @@ Application::Application(int &argc, char **argv) :
 
     parseOptions(arguments());
     setupTranslations();
-    // setupLogBrowser();
+    setupLogBrowser();
     //no need to waste time;
     if ( _helpOnly ) return;
 
