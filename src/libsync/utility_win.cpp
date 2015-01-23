2c25affb358d015b349b88dfd2e00dfa889f2c3d
Luca Lorenzetto <luca.lorenzetto@infocamere.it>
2015-01-23 10:54:50 +0100

    Better fix for #2719 supporting dynamic symbol loading

--- src/libsync/utility_win.cpp
+++ src/libsync/utility_win.cpp
@@ -10,42 +10,65 @@
  * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
  * for more details.
  */
-#define NTDDI_VERSION 0x06010000
 #include <shlobj.h>
 #include <winbase.h>
 #include <windows.h>
+#include <iostream>
+#include <string>
+#include <shlguid.h>
+
 
 static const char runPathC[] = "HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\Run";
 
-static bool isWinVistaOrHigher() {
-    OSVERSIONINFO osvi;
-    ZeroMemory(&osvi, sizeof(OSVERSIONINFO));
-    osvi.dwOSVersionInfoSize = sizeof(OSVERSIONINFO);
-    GetVersionEx(&osvi);
-    if (osvi.dwMajorVersion < 6) 
-       return false;
-    return true;
+typedef HRESULT (WINAPI* lpSHGetKnownFolderPath)(
+    REFKNOWNFOLDERID rfid,
+    DWORD dwFlags,
+    HANDLE hToken,
+    PWSTR *ppszPath
+);
 
-}
 
 static void setupFavLink_private(const QString &folder)
 {
     // Windows Explorer: Place under "Favorites" (Links)
     QString profile;
-    if (isWinVistaOrHigher()) {
-        wchar_t *path;
-        SHGetKnownFolderPath(FOLDERID_Links, 0, NULL, &path);
-        profile =  QDir::fromNativeSeparators(QString::fromWCharArray(path));
-    } else {
-        wchar_t path[MAX_PATH];
-        SHGetSpecialFolderPath(0, path, CSIDL_PROFILE, FALSE); 
-        profile =  QDir::fromNativeSeparators(QString::fromWCharArray(path));
+    bool hasKnownFolderPath = false;
+    HMODULE hndl_shell32;
+    lpSHGetKnownFolderPath pSHGetKnownFolderPath;
+
+    /* Always check the return value of LoadLibrary. */
+    hndl_shell32 = LoadLibrary(L"shell32");
+    if (NULL != hndl_shell32) {
+        pSHGetKnownFolderPath = (lpSHGetKnownFolderPath) GetProcAddress(hndl_shell32, "SHGetKnownFolderPath");
+
+        if(pSHGetKnownFolderPath != NULL) {
+            wchar_t *path;
+            if (SUCCEEDED(pSHGetKnownFolderPath(FOLDERID_Links, 0,NULL,&path))) {
+		hasKnownFolderPath = true;
+                profile =  QDir::fromNativeSeparators(QString::fromWCharArray(path));
+                CoTaskMemFree(path);
+            }
+        }
+        else {
+            // We're on .
+            wchar_t path[MAX_PATH];
+            SHGetSpecialFolderPath(0, path, CSIDL_PROFILE, FALSE);
+            profile =  QDir::fromNativeSeparators(QString::fromWCharArray(path));
+        }
+        FreeLibrary(hndl_shell32);
     }
+    else {
+	// In case we cannot load shell32.dll, choose legacy function that surely exists
+        wchar_t path[MAX_PATH];
+        SHGetSpecialFolderPath(0, path, CSIDL_PROFILE, FALSE);
+        profile =  QDir::fromNativeSeparators(QString::fromWCharArray(path));
+    } 
     QDir folderDir(QDir::fromNativeSeparators(folder));
-    if (isWinVistaOrHigher()) {
-        QString linkName = profile + QLatin1String("/") + folderDir.dirName() + QLatin1String(".lnk");
+    QString linkName;
+    if (hasKnownFolderPath) {
+        linkName = profile + QLatin1String("/") + folderDir.dirName() + QLatin1String(".lnk");
     } else {
-        QString linkName = profile+QLatin1String("/Links/") + folderDir.dirName() + QLatin1String(".lnk");
+        linkName = profile+QLatin1String("/Links/") + folderDir.dirName() + QLatin1String(".lnk");
     }
     if (!QFile::link(folder, linkName))
         qDebug() << Q_FUNC_INFO << "linking" << folder << "to" << linkName << "failed!";
