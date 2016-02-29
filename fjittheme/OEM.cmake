fee9084d43c3a3dbaf2a0f8e2f45dadb5c74c224
Sigvald Høyheim <shoyheim@gmail.com>
2016-02-29 13:18:42 +0100

    implementing basic Serit Fjordane IT theme in the 2.1.1 version of the client

---
+++ fjittheme/OEM.cmake
@@ -0,0 +1,18 @@
+set( APPLICATION_NAME       "Skylagring" )
+set( APPLICATION_EXECUTABLE "Skylagring" )
+set( APPLICATION_DOMAIN     "fil.fjit.no" )
+set( APPLICATION_VENDOR     "Serit Fjordane IT AS" )
+set( APPLICATION_UPDATE_URL "https://fil.fjit.no/install/client/" CACHE string "URL for updater" )
+
+set( THEME_CLASS            "MyTheme" )
+set( APPLICATION_REV_DOMAIN "no.fjit.fil.desktopclient" )
+set( WIN_SETUP_BITMAP_PATH  "${OEM_THEME_DIR}/resources" )
+
+set( CPACK_PACKAGE_ICON  "${OEM_THEME_DIR}/resources/skylagring.ico" )
+
+# set( MAC_INSTALLER_BACKGROUND_FILE "${CMAKE_SOURCE_DIR}/admin/osx/installer-background.png" CACHE STRING "The MacOSX installer background image")
+
+set( THEME_INCLUDE          "${OEM_THEME_DIR}/mytheme.h" )
+# set( APPLICATION_LICENSE    "${OEM_THEME_DIR}/license.txt )
+
+set( MIRALL_VERSION_SUFFIX "_fjit")
---
+++ fjittheme/mytheme.h
@@ -0,0 +1,91 @@
+/*
+ * Copyright (C) by Daniel Molkentin <danimo@owncloud.com>, ownCloud Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ */
+
+#ifndef MY_THEME_H
+#define MY_THEME_H
+
+#include "theme.h"
+
+#include <QString>
+#include <QDebug>
+#include <QPixmap>
+#include <QIcon>
+#include <QApplication>
+
+
+namespace OCC {
+
+class MyTheme : public Theme
+{
+public:
+    MyTheme() : Theme() {}
+
+    QIcon folderDisabledIcon() const Q_DECL_OVERRIDE { return themeIcon( QLatin1String("state-error" ) ); }
+    QString configFileName() const Q_DECL_OVERRIDE  { return QLatin1String( "skylagring.cfg" ); }
+
+    /*
+     * If you changed file names in /replacements/mytheme/theme/colored
+     * you will have to do that here also.
+     */
+    QIcon applicationIcon() const Q_DECL_OVERRIDE {
+        return themeIcon( QLatin1String("Skylagring-icon" ) );
+    }
+
+    /*
+     * If you have your own help page change the URL below.
+     * example: return QLatin1String( "http://www.yourwebsite.com/support" );
+     */
+    virtual QString helpUrl() const {
+        return QString::fromLatin1("http://doc.owncloud.org/desktop/%1.%2/").arg(MIRALL_VERSION_MAJOR).arg(MIRALL_VERSION_MINOR);
+    }
+
+    /*
+     * If you have your own URL for updates, change it below.
+     * example: return QLatin1String( "http://www.yourwebsite.com/updates/" );
+     */
+    virtual QString updateCheckUrl() const {
+        return QLatin1String( "https://fil.fjit.no/install/client/" );
+    }
+
+    /*
+     * Enter the color of the header title in the setup wizard.
+     * example: return QColor("#FFFFFF");
+     */
+	virtual QColor wizardHeaderTitleColor() const
+	{
+	    return QColor("#D36D2F");
+	}
+
+    /*
+     * Enter the background color of the header in the setup wizard.
+     * example: return QColor("#10283F");
+     */
+	virtual QColor wizardHeaderBackgroundColor() const
+	{
+	    return QColor("#000000");
+	}
+
+    /*
+     * The logo you want to be seen in the header of the setup wizard..
+     * example: return QPixmap(":/mirall/theme/colored/wizard_logo.png");
+     */
+	virtual QPixmap wizardHeaderLogo() const
+	{
+	    return QPixmap(":/fjittheme/theme/colored/wizard_logo.png");
+	}
+
+};
+
+} // namespace mirall
+
+#endif // MY_THEME_H
