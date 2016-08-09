f8a81849d9ac851aa47516d6820f6d8395c6fdc2
Sigvald Høyheim <sigvald.hoyheim@fjordane-it.no>
2016-08-09 15:19:49 +0200

    Ensure selecting navigation pane target updates all parts of registry

--- src/libsync/utility_win.cpp
+++ src/libsync/utility_win.cpp
@@ -18,6 +18,7 @@
 
 static const char runPathC[] = "HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\Run";
 static const char panePathC[] = "Software\\Classes\\CLSID\\{XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX}\\Instance\\InitPropertyBag";
+static const char panePath2C[] = "Software\\Classes\\CLSID\\Wow6432Node\\{XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX}\\Instance\\InitPropertyBag";
 
 static bool CreateRegistryKeyValue( HKEY hKeyParent, PWCHAR KeyName, PWCHAR ValueName, PBYTE pValue,  DWORD dwValueSize )
 {
@@ -82,6 +83,10 @@ static void updateNavPanel_private(const QString &folder)
     panePath.replace("{XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX}", QLatin1String(APPLICATION_CLSID));
     wchar_t* newPanePath = new wchar_t[panePath.length()+1]();
     panePath.toWCharArray(newPanePath);
+    QString panePath2 = QLatin1String(panePath2C);
+    panePath2.replace("{XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX}", QLatin1String(APPLICATION_CLSID));
+    wchar_t* newPanePath2 = new wchar_t[panePath2.length()+1]();
+    panePath2.toWCharArray(newPanePath2);
     QString f = folder;
     f.replace('/','\\');
     wchar_t* path = new wchar_t[f.length()+1]();
@@ -90,6 +95,7 @@ static void updateNavPanel_private(const QString &folder)
     wcsncpy(valuename, L"TargetFolderPath", 16);
 
     CreateRegistryKeyValue ( HKEY_CURRENT_USER, newPanePath, valuename, (PBYTE)path, sizeof(wchar_t) * f.length());
+    CreateRegistryKeyValue ( HKEY_CURRENT_USER, newPanePath2, valuename, (PBYTE)path, sizeof(wchar_t) * f.length());
 
     // memory cleanup
     delete newPanePath;
