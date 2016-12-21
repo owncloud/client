nextcloud/desktop

commit a932eac832b442cca763197240f036905dd284da

Multi-account WIP

src/gui/owncloudsetupwizard.cpp
@@ -454,14 +454,6 @@ bool OwncloudSetupWizard::ensureStartFromScratch(const QString &localFolder) {
     return renameOk;
 }
 
-void OwncloudSetupWizard::replaceDefaultAccountWith(AccountPtr newAccount)
-{
-    // new Account
-    AccountManager *mgr = AccountManager::instance();
-    mgr->setAccount(newAccount);
-    mgr->save();
-}
-
 // Method executed when the user ends has finished the basic setup.
 void OwncloudSetupWizard::slotAssistantFinished( int result )
 {
@@ -511,26 +503,9 @@ void OwncloudSetupWizard::slotSkipFolderConfiguration()
 void OwncloudSetupWizard::applyAccountChanges()
 {
     AccountPtr newAccount = _ocWizard->account();
-    AccountPtr origAccount = AccountManager::instance()->account();
+    auto manager = AccountManager::instance();
 
-    bool isInitialSetup = (origAccount == 0);
-
-    // check if either the account changed in a major way
-    bool reinitRequired =
-            newAccount->changed(origAccount, true /* ignoreProtocol, allows http->https */);
-
-    // If this is a completely new account, replace it entirely
-    // thereby clearing all folder definitions.
-    if (isInitialSetup || reinitRequired) {
-        replaceDefaultAccountWith(newAccount);
-        qDebug() << "Significant changes or first setup: switched to new account";
-    }
-    // Otherwise, set only URL and credentials
-    else {
-        origAccount->setUrl(newAccount->url());
-        origAccount->setCredentials(_ocWizard->getCredentials());
-        qDebug() << "Only password or schema was changed, adjusted existing account";
-    }
+    manager->addAccount(newAccount);
 }
 
 

src/gui/accountmanager.cpp
@@ -12,6 +12,7 @@
  */
 
 #include "accountmanager.h"
+#include "sslerrordialog.h"
 #include <theme.h>
 #include <creds/credentialsfactory.h>
 #include <creds/abstractcredentials.h>
@@ -35,17 +36,6 @@ AccountManager *AccountManager::instance()
 {
     static AccountManager instance;
     return &instance;
-}
-
-void AccountManager::setAccount(AccountPtr account)
-{
-    if (_account) {
-        emit accountRemoved(_account);
-    }
-    _account = account;
-    if (account) {
-        emit accountAdded(account);
-    }
 }
 
 bool AccountManager::restore()
@@ -113,7 +103,8 @@ bool AccountManager::restore()
         settings->beginGroup(QLatin1String("General"));
         acc->setApprovedCerts(QSslCertificate::fromData(settings->value(caCertsKeyC).toByteArray()));
         acc->setMigrated(migratedCreds);
-        setAccount(acc);
+        acc->setSslErrorHandler(new SslDialogErrorHandler);
+        addAccount(acc);
         return true;
     }
     return false;
@@ -122,8 +113,13 @@ bool AccountManager::restore()
 
 void AccountManager::save()
 {
-    auto acc = account();
-    if (!acc) { return; }
+    foreach (const auto &acc , _accounts) {
+        save(acc->account());
+    }
+}
+
+void AccountManager::save(const AccountPtr& acc)
+{
     QScopedPointer<QSettings> settings(Account::settingsWithGroup(Theme::instance()->appName()));
     settings->setValue(QLatin1String(urlC), acc->_url.toString());
     if (acc->_credentials) {
@@ -157,6 +153,22 @@ void AccountManager::save()
             qDebug() << "Saving cookies.";
             jar->save();
         }
+    }
+}
+
+void AccountManager::addAccount(const AccountPtr& newAccount)
+{
+    AccountStatePtr newAccountState(new AccountState(newAccount));
+    _accounts << newAccountState;
+    emit accountAdded(newAccountState.data());
+}
+
+void AccountManager::shutdown()
+{
+    auto accountsCopy = _accounts;
+    _accounts.clear();
+    foreach (const auto &acc, accountsCopy) {
+        emit accountRemoved(acc.data());
     }
 }
 

src/gui/accountmanager.h
@@ -14,17 +14,17 @@
 #pragma once
 
 #include "account.h"
+#include "accountstate.h"
 
 namespace OCC {
+
+typedef QSharedPointer<AccountState> AccountStatePtr;
 
 class OWNCLOUDSYNC_EXPORT AccountManager : public QObject {
     Q_OBJECT
 public:
     static AccountManager *instance();
     ~AccountManager() {}
-
-    void setAccount(AccountPtr account);
-    AccountPtr account() { return _account; }
 
     /**
      * Saves the account to a given settings file
@@ -37,13 +37,30 @@ public:
      */
     bool restore();
 
+    /**
+     * Add this account in the list of saved account.
+     * Typically called from the wizard
+     */
+    void addAccount(const AccountPtr &newAccount);
+
+    /**
+     * remove all accounts
+     */
+    void shutdown();
+
+    QList<AccountStatePtr> accounts() { return _accounts; }
+
+private:
+    void save(const AccountPtr &account);
+
+
 Q_SIGNALS:
-    void accountAdded(AccountPtr account);
-    void accountRemoved(AccountPtr account);
+    void accountAdded(AccountState *account);
+    void accountRemoved(AccountState *account);
 
 private:
     AccountManager() {}
-    AccountPtr _account;
+    QList<AccountStatePtr> _accounts;
 };
 
 }

src/gui/accountsettings.cpp
@@ -26,7 +26,6 @@
 #include "accountstate.h"
 #include "quotainfo.h"
 #include "creds/abstractcredentials.h"
-#include "accountmanager.h"
 
 #include <math.h>
 
@@ -57,11 +56,11 @@ static const char progressBarStyleC[] =
         "background-color: %1; width: 1px;"
         "}";
 
-AccountSettings::AccountSettings(QWidget *parent) :
+AccountSettings::AccountSettings(AccountState *accountState, QWidget *parent) :
     QWidget(parent),
     ui(new Ui::AccountSettings),
     _wasDisabledBefore(false),
-    _accountState(AccountStateManager::instance()->accountState())
+    _accountState(accountState)
 {
     ui->setupUi(this);
 
@@ -119,9 +118,17 @@ AccountSettings::AccountSettings(QWidget *parent) :
 
     ui->connectLabel->setText(tr("No account configured."));
 
-    connect(AccountStateManager::instance(), SIGNAL(accountStateAdded(AccountState*)),
-            this, SLOT(slotAccountStateChanged(AccountState*)));
-    slotAccountStateChanged(AccountStateManager::instance()->accountState());
+    connect(_accountState, SIGNAL(stateChanged(int)), SLOT(slotAccountStateChanged(int)));
+    slotAccountStateChanged(_accountState->state());
+
+    QuotaInfo *quotaInfo = _accountState->quotaInfo();
+    connect( quotaInfo, SIGNAL(quotaUpdated(qint64,qint64)),
+            this, SLOT(slotUpdateQuota(qint64,qint64)));
+    slotUpdateQuota(quotaInfo->lastQuotaTotalBytes(), quotaInfo->lastQuotaUsedBytes());
+
+    connect( ProgressDispatcher::instance(), SIGNAL(progressInfo(QString, Progress::Info)),
+             this, SLOT(slotSetProgress(QString, Progress::Info)) );
+
 }
 
 void AccountSettings::slotCustomContextMenuRequested(const QPoint &pos)
@@ -149,28 +156,6 @@ void AccountSettings::slotCustomContextMenuRequested(const QPoint &pos)
     menu->exec(tv->mapToGlobal(pos));
 }
 
-
-void AccountSettings::slotAccountStateChanged(AccountState *newAccountState)
-{
-    if (_accountState) {
-        disconnect(_accountState, SIGNAL(stateChanged(int)), this, SLOT(slotAccountStateChanged(int)));
-        disconnect(_accountState->quotaInfo(), SIGNAL(quotaUpdated(qint64,qint64)),
-                    this, SLOT(slotUpdateQuota(qint64,qint64)));
-        disconnect(_accountState, SIGNAL(stateChanged(int)), this, SLOT(slotAccountStateChanged(int)));
-    }
-
-    _accountState = newAccountState;
-    if (_accountState) {
-        connect(_accountState, SIGNAL(stateChanged(int)), SLOT(slotAccountStateChanged(int)));
-        slotAccountStateChanged(_accountState->state());
-
-        QuotaInfo *quotaInfo = _accountState->quotaInfo();
-        connect( quotaInfo, SIGNAL(quotaUpdated(qint64,qint64)),
-                this, SLOT(slotUpdateQuota(qint64,qint64)));
-        slotUpdateQuota(quotaInfo->lastQuotaTotalBytes(), quotaInfo->lastQuotaUsedBytes());
-    }
-}
-
 void AccountSettings::slotFolderActivated( const QModelIndex& indx )
 {
     if (indx.data(FolderStatusDelegate::AddButton).toBool()) {
@@ -184,7 +169,7 @@ void AccountSettings::slotAddFolder()
     FolderMan *folderMan = FolderMan::instance();
     folderMan->setSyncEnabled(false); // do not start more syncs.
 
-    FolderWizard *folderWizard = new FolderWizard(AccountManager::instance()->account(), this);
+    FolderWizard *folderWizard = new FolderWizard(_accountState->account(), this);
 
     connect(folderWizard, SIGNAL(accepted()), SLOT(slotFolderWizardAccepted()));
     connect(folderWizard, SIGNAL(rejected()), SLOT(slotFolderWizardRejected()));

src/gui/accountsettings.h
@@ -45,7 +45,7 @@ class AccountSettings : public QWidget
     Q_OBJECT
 
 public:
-    explicit AccountSettings(QWidget *parent = 0);
+    explicit AccountSettings(AccountState *accountState, QWidget *parent = 0);
     ~AccountSettings();
 
 
@@ -66,6 +66,7 @@ public slots:
     void slotAccountStateChanged(int state);
 
     void setGeneralErrors( const QStringList& errors );
+    AccountState* accountsState() { return _accountState; };
 
 protected slots:
     void slotAddFolder();
@@ -92,7 +93,6 @@ private:
     QLabel *_quotaLabel;
 private slots:
     void slotFolderSyncStateChange();
-    void slotAccountStateChanged(AccountState*);
     void slotCustomContextMenuRequested(const QPoint&);
 };
 

src/gui/accountstate.cpp
@@ -21,39 +21,6 @@
 
 namespace OCC {
 
-Q_GLOBAL_STATIC(AccountStateManager, g_accountStateManager)
-
-AccountStateManager *AccountStateManager::instance()
-{
-    return g_accountStateManager();
-}
-
-AccountStateManager::AccountStateManager()
-    : _accountState(0)
-{
-    connect(AccountManager::instance(), SIGNAL(accountAdded(AccountPtr)),
-            SLOT(slotAccountAdded(AccountPtr)));
-}
-
-AccountStateManager::~AccountStateManager()
-{}
-
-void AccountStateManager::setAccountState(AccountState *accountState)
-{
-    if (_accountState) {
-        emit accountStateRemoved(_accountState);
-    }
-    _accountState = accountState;
-    if (accountState) {
-        emit accountStateAdded(accountState);
-    }
-}
-
-void AccountStateManager::slotAccountAdded(AccountPtr account)
-{
-    setAccountState(new AccountState(account));
-}
-
 AccountState::AccountState(AccountPtr account)
     : QObject(account.data())
     , _account(account)
@@ -78,7 +45,7 @@ AccountState::~AccountState()
 
 AccountPtr AccountState::account() const
 {
-    return _account.toStrongRef();
+    return _account;
 }
 
 AccountState::ConnectionStatus AccountState::connectionStatus() const
@@ -266,5 +233,13 @@ void AccountState::slotCredentialsFetched(AbstractCredentials* credentials)
 
     checkConnectivity();
 }
+
+QString AccountState::displayName()
+{
+    auto user = account()->credentials()->user();
+    auto url = account()->url();
+    return tr("%1@%2").arg(user, url.host());
+}
+
 
 } // namespace OCC

src/gui/accountstate.h
@@ -27,29 +27,6 @@ class AccountState;
 class Account;
 class AbstractCredentials;
 
-class AccountStateManager : public QObject {
-    Q_OBJECT
-public:
-    static AccountStateManager *instance();
-
-    AccountStateManager();
-    ~AccountStateManager();
-
-    AccountState *accountState() { return _accountState; }
-
-signals:
-    void accountStateAdded(AccountState *accountState);
-    void accountStateRemoved(AccountState *accountState);
-
-private slots:
-    void slotAccountAdded(AccountPtr account);
-
-private:
-    void setAccountState(AccountState *account);
-
-    AccountState *_accountState;
-};
-
 /**
  * @brief Extra info about an ownCloud server account.
  */
@@ -108,6 +85,9 @@ public:
     /// connection status and errors.
     void checkConnectivity();
 
+    // The name of the account as shown in the toolbar
+    QString displayName();
+
 private:
     void setState(State state);
 
@@ -120,9 +100,7 @@ protected Q_SLOTS:
     void slotCredentialsFetched(AbstractCredentials* creds);
 
 private:
-    // A strong reference here would keep Account and AccountState
-    // alive indefinitely since Account is the parent of AccountState.
-    QWeakPointer<Account> _account;
+    AccountPtr _account;
     QuotaInfo *_quotaInfo;
     State _state;
     ConnectionStatus _connectionStatus;

src/gui/application.cpp
@@ -123,13 +123,7 @@ Application::Application(int &argc, char **argv) :
 
     connect(this, SIGNAL(messageReceived(QString, QObject*)), SLOT(slotParseMessage(QString, QObject*)));
 
-    // Create the account info manager to ensure it's listening to the
-    // account manager.
-    AccountStateManager::instance();
-
-    if (AccountManager::instance()->restore()) {
-        AccountManager::instance()->account()->setSslErrorHandler(new SslDialogErrorHandler);
-    }
+    AccountManager::instance()->restore();
 
     FolderMan::instance()->setSyncEnabled(false);
 
@@ -149,12 +143,12 @@ Application::Application(int &argc, char **argv) :
         _gui->slotToggleLogBrowser(); // _showLogWindow is set in parseOptions.
     }
 
-    connect(AccountStateManager::instance(), SIGNAL(accountStateAdded(AccountState*)),
+    connect(AccountManager::instance(), SIGNAL(accountAdded(AccountState*)),
             SLOT(slotAccountStateAdded(AccountState*)));
-    connect(AccountStateManager::instance(), SIGNAL(accountStateRemoved(AccountState*)),
+    connect(AccountManager::instance(), SIGNAL(accountRemoved(AccountState*)),
             SLOT(slotAccountStateRemoved(AccountState*)));
-    if (AccountState *ai = AccountStateManager::instance()->accountState()) {
-        slotAccountStateAdded(ai);
+    foreach (auto ai , AccountManager::instance()->accounts()) {
+        slotAccountStateAdded(ai.data());
     }
 
     connect(FolderMan::instance()->socketApi(), SIGNAL(shareCommandReceived(QString, QString, bool)),
@@ -180,22 +174,26 @@ Application::Application(int &argc, char **argv) :
 Application::~Application()
 {
     // Remove the account from the account manager so it can be deleted.
-    AccountManager::instance()->setAccount(AccountPtr());
+    AccountManager::instance()->shutdown();
 }
 
 void Application::slotLogin()
 {
-    AccountState *a = AccountStateManager::instance()->accountState();
-    if (a) {
+    auto list = AccountManager::instance()->accounts();
+    if (!list.isEmpty()) {
         FolderMan::instance()->setupFolders();
+    }
+
+    foreach (const auto &a, list) {
         a->setSignedOut(false);
     }
 }
 
 void Application::slotLogout()
 {
-    AccountState* ai = AccountStateManager::instance()->accountState();
-    if (ai) {
+    auto list = AccountManager::instance()->accounts();
+
+    foreach (const auto &ai, list) {
         AccountPtr a = ai->account();
         // invalidate & forget token/password
         a->credentials()->invalidateToken();
@@ -206,6 +204,9 @@ void Application::slotLogout()
         ai->setSignedOut(true);
         // show result
         _gui->slotComputeOverallSyncStatus();
+    }
+    if (!list.isEmpty()) {
+        FolderMan::instance()->setupFolders();
     }
 }
 
@@ -244,12 +245,12 @@ void Application::slotStartUpdateDetector()
 
 void Application::slotCheckConnection()
 {
-    AccountState *accountState = AccountStateManager::instance()->accountState();
-
-    if( accountState ) {
+    auto list = AccountManager::instance()->accounts();
+    foreach (const auto &accountState , list) {
         accountState->checkConnectivity();
+    }
 
-    } else {
+    if (list.isEmpty()) {
         // let gui open the setup wizard
         _gui->slotOpenSettingsDialog( true );
 
@@ -301,10 +302,13 @@ void Application::slotUpdateConnectionErrors(int accountState)
         _startupNetworkError = accountState == AccountState::NetworkError;
     }
 
+#warning FIXME
+#if 0
     AccountState *as = AccountStateManager::instance()->accountState();
     if (as) {
         _gui->setConnectionErrors( isConnected, as->connectionErrors() );
     }
+#endif
 }
 
 void Application::slotownCloudWizardDone( int res )

owncloud/client

commit c2fccd4afb686ded135f9ae98eef70a32d40c050

Wizard: Detach saved account from wizard
Fixes #5408 #5407.

The problem was that cleanup of the credentials page set the
credentials of the account back to dummy, thereby overriding
things like shib usernames.

This should be broken since a932eac832b442cca763197240f036905dd284da.

src/gui/owncloudsetupwizard.cpp
@@ -553,6 +553,13 @@ void OwncloudSetupWizard::slotSkipFolderConfiguration()
 AccountState *OwncloudSetupWizard::applyAccountChanges()
 {
     AccountPtr newAccount = _ocWizard->account();
+
+    // Detach the account that is going to be saved from the
+    // wizard to ensure it doesn't accidentally get modified
+    // later (such as from running cleanup such as
+    // AbstractCredentialsWizardPage::cleanupPage())
+    _ocWizard->setAccount(AccountManager::createAccount());
+
     auto manager = AccountManager::instance();
 
     auto newState = manager->addAccount(newAccount);