nextcloud/desktop

commit e1ca612c5d3087e02f6b7f8e454224e0b88e82ad

Don't insert items into the folder model if selectiveSync is not supported
This fixes an assertion in FolderStatusModel::SubFolderInfo::resetSubs
rowCount reported 0 but we actually had items in the model

src/gui/folderstatusmodel.cpp
@@ -619,6 +619,9 @@ void FolderStatusModel::slotUpdateDirectories(const QStringList &list)
     if (!parentInfo) {
         return;
     }
+    if (!parentInfo->_folder->supportsSelectiveSync()) {
+        return;
+    }
     ASSERT(parentInfo->_fetchingJob == job);
     ASSERT(parentInfo->_subs.isEmpty());
 

owncloud/client

commit 00742331cb23a90a10dc4c5717d8df516abc8528

Fix assertion introduced by e1ca612c5d3087e02f6b7f8e454224e0b88e82ad
[ fatal default ]:ASSERT: "last < rowCount(parent)" in file C:\_\17a9f6ae\qtbase-everywhere-src-5.12.9\src\corelib\itemmodels\qabstractitemmodel.cpp, line 2787

e1ca612c5d3087e02f6b7f8e454224e0b88e82ad stopped adding sub folders so we can return the actual size.

src/gui/folderstatusmodel.cpp
@@ -362,8 +362,6 @@ int FolderStatusModel::rowCount(const QModelIndex &parent) const
     auto info = infoForIndex(parent);
     if (!info)
         return 0;
-    if (info->_folder && !info->_folder->supportsSelectiveSync())
-        return 0;
     if (info->hasLabel())
         return 1;
     return info->_subs.count();