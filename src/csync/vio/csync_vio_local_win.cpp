nextcloud/desktop

commit dd641fae997d71c8396b77def2fa25ad96fdf47f

Remove custome string functions

src/csync/vio/csync_vio_local_win.cpp
@@ -29,7 +29,6 @@
 
 #include "c_private.h"
 #include "c_lib.h"
-#include "c_utf8.h"
 #include "csync_util.h"
 #include "vio/csync_vio_local.h"
 #include "common/filesystembase.h"
@@ -135,12 +134,12 @@ std::unique_ptr<csync_file_stat_t> csync_vio_local_readdir(csync_vio_handle_t *h
           return nullptr;
       }
   }
-  auto path = c_utf8_from_locale(handle->ffd.cFileName);
+  auto path = QString::fromWCharArray(handle->ffd.cFileName);
   if (path == "." || path == "..")
       return csync_vio_local_readdir(handle, vfs);
 
   file_stat.reset(new csync_file_stat_t);
-  file_stat->path = path;
+  file_stat->path = path.toUtf8();
 
     if (vfs && vfs->statTypeVirtualFile(file_stat.get(), &handle->ffd)) {
       // all good
@@ -175,7 +174,7 @@ std::unique_ptr<csync_file_stat_t> csync_vio_local_readdir(csync_vio_handle_t *h
 
     std::wstring fullPath;
     fullPath.reserve(handle->path.size() + std::wcslen(handle->ffd.cFileName));
-    fullPath += reinterpret_cast<const wchar_t *>(handle->path.utf16()); // path always ends with '\', by construction
+    fullPath += handle->path.toStdWString(); // path always ends with '\', by construction
     fullPath += handle->ffd.cFileName;
 
     if (_csync_vio_local_stat_mb(fullPath.data(), file_stat.get()) < 0) {
@@ -187,11 +186,10 @@ std::unique_ptr<csync_file_stat_t> csync_vio_local_readdir(csync_vio_handle_t *h
 }
 
 
-int csync_vio_local_stat(const char *uri, csync_file_stat_t *buf)
+int csync_vio_local_stat(const QString &uri, csync_file_stat_t *buf)
 {
-    mbchar_t *wuri = c_utf8_path_to_locale(uri);
-    int rc = _csync_vio_local_stat_mb(wuri, buf);
-    c_free_locale_string(wuri);
+    const std::wstring wuri = uri.toStdWString();
+    int rc = _csync_vio_local_stat_mb(wuri.data(), buf);
     return rc;
 }
 

src/libsync/filesystem.cpp
@@ -21,13 +21,10 @@
 #include <QDirIterator>
 #include <QCoreApplication>
 
-// We use some internals of csync:
-extern "C" int c_utimes(const char *, const struct timeval *);
-
 #include "csync.h"
 #include "vio/csync_vio_local.h"
 #include "std/c_string.h"
-#include "std/c_utf8.h"
+#include "std/c_time.h"
 
 #ifdef Q_OS_WIN32
 #include <winsock2.h>
@@ -72,7 +69,7 @@ time_t FileSystem::getModTime(const QString &filename)
 {
     csync_file_stat_t stat;
     qint64 result = -1;
-    if (csync_vio_local_stat(filename.toUtf8().data(), &stat) != -1
+    if (csync_vio_local_stat(filename, &stat) != -1
         && (stat.modtime != 0)) {
         result = stat.modtime;
     } else {
@@ -88,7 +85,7 @@ bool FileSystem::setModTime(const QString &filename, time_t modTime)
     struct timeval times[2];
     times[0].tv_sec = times[1].tv_sec = modTime;
     times[0].tv_usec = times[1].tv_usec = 0;
-    int rc = c_utimes(filename.toUtf8().data(), times);
+    int rc = c_utimes(filename, times);
     if (rc != 0) {
         qCWarning(lcFileSystem) << "Error setting mtime for" << filename
                                 << "failed: rc" << rc << ", errno:" << errno;
@@ -125,7 +122,7 @@ static qint64 getSizeWithCsync(const QString &filename)
 {
     qint64 result = 0;
     csync_file_stat_t stat;
-    if (csync_vio_local_stat(filename.toUtf8().data(), &stat) != -1) {
+    if (csync_vio_local_stat(filename, &stat) != -1) {
         result = stat.size;
     } else {
         qCWarning(lcFileSystem) << "Could not get size for" << filename << "with csync";
@@ -196,7 +193,7 @@ bool FileSystem::removeRecursively(const QString &path, const std::function<void
 bool FileSystem::getInode(const QString &filename, quint64 *inode)
 {
     csync_file_stat_t fs;
-    if (csync_vio_local_stat(filename.toUtf8().constData(), &fs) == 0) {
+    if (csync_vio_local_stat(filename, &fs) == 0) {
         *inode = fs.inode;
         return true;
     }

src/csync/CMakeLists.txt
@@ -28,8 +28,7 @@ set(csync_SRCS
 
   std/c_alloc.c
   std/c_string.c
-  std/c_time.c
-  std/c_utf8.cpp
+  std/c_time.cpp
 
 )
 

src/csync/csync_exclude.cpp
@@ -26,7 +26,6 @@
 
 #include "c_lib.h"
 #include "c_private.h"
-#include "c_utf8.h"
 
 #include "csync_exclude.h"
 

src/csync/std/c_lib.h
@@ -24,5 +24,4 @@
 #include "c_macro.h"
 #include "c_alloc.h"
 #include "c_string.h"
-#include "c_time.h"
 #include "c_private.h"

src/csync/std/c_time.cpp
@@ -23,13 +23,12 @@
 #include "c_string.h"
 
 #include "c_time.h"
-#include "c_utf8.h"
+
+#include <QFile>
 
 #ifdef HAVE_UTIMES
-int c_utimes(const char *uri, const struct timeval *times) {
-    mbchar_t *wuri = c_utf8_path_to_locale(uri);
-    int ret = utimes(wuri, times);
-    c_free_locale_string(wuri);
+int c_utimes(const QString &uri, const struct timeval *times) {
+    int ret = utimes(QFile::encodeName(uri).constData(), times);
     return ret;
 }
 #else // HAVE_UTIMES
@@ -51,12 +50,12 @@ static void UnixTimevalToFileTime(struct timeval t, LPFILETIME pft)
     pft->dwHighDateTime = ll >> 32;
 }
 
-int c_utimes(const char *uri, const struct timeval *times) {
+int c_utimes(const QString &uri, const struct timeval *times) {
     FILETIME LastAccessTime;
     FILETIME LastModificationTime;
     HANDLE hFile;
 
-    mbchar_t *wuri = c_utf8_path_to_locale( uri );
+    auto wuri = uri.toStdWString();
 
     if(times) {
         UnixTimevalToFileTime(times[0], &LastAccessTime);
@@ -67,7 +66,7 @@ int c_utimes(const char *uri, const struct timeval *times) {
         GetSystemTimeAsFileTime(&LastModificationTime);
     }
 
-    hFile=CreateFileW(wuri, FILE_WRITE_ATTRIBUTES, FILE_SHARE_DELETE | FILE_SHARE_READ | FILE_SHARE_WRITE,
+    hFile=CreateFileW(wuri.data(), FILE_WRITE_ATTRIBUTES, FILE_SHARE_DELETE | FILE_SHARE_READ | FILE_SHARE_WRITE,
                       NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL+FILE_FLAG_BACKUP_SEMANTICS, NULL);
     if(hFile==INVALID_HANDLE_VALUE) {
         switch(GetLastError()) {
@@ -95,12 +94,10 @@ int c_utimes(const char *uri, const struct timeval *times) {
         //can this happen?
         errno=ENOENT;
         CloseHandle(hFile);
-        c_free_locale_string(wuri);
         return -1;
     }
 
     CloseHandle(hFile);
-    c_free_locale_string(wuri);
 
     return 0;
 }

src/csync/std/c_time.h
@@ -21,11 +21,9 @@
 #ifndef _C_TIME_H
 #define _C_TIME_H
 
-#include "ocsynclib.h"
+#include <QString>
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+#include "ocsynclib.h"
 
 #ifdef _WIN32
 #include <time.h>
@@ -33,10 +31,7 @@ extern "C" {
 #include <sys/time.h>
 #endif
 
-OCSYNC_EXPORT int c_utimes(const char *uri, const struct timeval *times);
+OCSYNC_EXPORT int c_utimes(const QString &uri, const struct timeval *times);
 
-#ifdef __cplusplus
-}
-#endif
 
 #endif /* _C_TIME_H */

src/csync/std/c_utf8.cpp
@@ -1,126 +1,0 @@
-/*
- * cynapses libc functions
- *
- * Copyright (c) 2008-2013 by Andreas Schneider <asn@cryptomilk.org>
- * Copyright (c) 2012-2013 by Klaas Freitag <freitag@owncloud.com>
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
- */
-
-#include "config_csync.h"
-#include "c_utf8.h"
-
-#ifdef _WIN32
-#include <ctype.h>
-#include <string.h>
-#include <stdlib.h>
-#include <limits.h>
-#include <sys/types.h>
-#include <wchar.h>
-#include <windows.h>
-#else
-#include <QtCore/QTextCodec>
-#include <QtCore/QFile>
-#endif
-
-#include "c_alloc.h"
-#include "c_string.h"
-#include "common/filesystembase.h"
-
-/* Convert a locale String to UTF8 */
-QByteArray c_utf8_from_locale(const mbchar_t *wstr)
-{
-  if (wstr == nullptr) {
-    return QByteArray();
-  }
-
-#ifdef _WIN32
-  QByteArray dst;
-  int size_needed;
-  size_t len;
-  len = wcslen(wstr);
-  /* Call once to get the required size. */
-  size_needed = WideCharToMultiByte(CP_UTF8, 0, wstr, len, NULL, 0, NULL, NULL);
-  if (size_needed > 0) {
-    dst.resize(size_needed);
-    WideCharToMultiByte(CP_UTF8, 0, wstr, len, dst.data(), size_needed, NULL, NULL);
-  }
-  return dst;
-#else
-    auto codec = QTextCodec::codecForLocale();
-#ifndef __APPLE__
-    if (codec->mibEnum() == 106) { // UTF-8
-        // Optimisation for UTF-8: no need to convert to QString.
-        // We still need to do it for mac because of normalization
-        return QByteArray(wstr);
-    }
-#endif
-    QTextDecoder dec(codec);
-    QString s = dec.toUnicode(wstr, qstrlen(wstr));
-    if (s.isEmpty() || dec.hasFailure()) {
-        /* Conversion error: since we can't report error from this function, just return the original
-            string.  We take care of invalid utf-8 in SyncEngine::treewalkFile */
-        return QByteArray(wstr);
-    }
-#ifdef __APPLE__
-    s = s.normalized(QString::NormalizationForm_C);
-#endif
-    return std::move(s).toUtf8();
-#endif
-}
-
-extern "C" {
-
-/* Convert a an UTF8 string to locale */
-mbchar_t* c_utf8_string_to_locale(const char *str)
-{
-    if (str == nullptr ) {
-        return nullptr;
-    }
-#ifdef _WIN32
-    mbchar_t *dst = NULL;
-    size_t len;
-    int size_needed;
-
-    len = strlen(str);
-    size_needed = MultiByteToWideChar(CP_UTF8, 0, str, len, NULL, 0);
-    if (size_needed > 0) {
-        int size_char = (size_needed + 1) * sizeof(mbchar_t);
-        dst = (mbchar_t*)c_malloc(size_char);
-        memset((void*)dst, 0, size_char);
-        MultiByteToWideChar(CP_UTF8, 0, str, -1, dst, size_needed);
-    }
-    return dst;
-#else
-    return c_strdup(QFile::encodeName(QString::fromUtf8(str)));
-#endif
-}
-
- mbchar_t* c_utf8_path_to_locale(const char *str)
- {
-     if( str == nullptr ) {
-         return nullptr;
-     } else {
- #ifdef _WIN32
-         QByteArray unc_str = OCC::FileSystem::pathtoUNC(QByteArray::fromRawData(str, strlen(str)));
-         mbchar_t *dst = c_utf8_string_to_locale(unc_str);
-         return dst;
- #else
-         return c_utf8_string_to_locale(str);
- #endif
-     }
- }
-
-}

owncloud/client

commit 48b6f1d2eeaa25078e502bc6810a5c144be35171

Fix Windows long path issue introduced in dd641fae997d71c8396b77def2fa25ad96fdf47f

src/csync/vio/csync_vio_local_win.cpp
@@ -32,6 +32,7 @@
 #include "csync_util.h"
 #include "vio/csync_vio_local.h"
 #include "common/filesystembase.h"
+#include "common/utility.h"
 
 #include <QtCore/QLoggingCategory>
 
@@ -49,8 +50,6 @@ struct csync_vio_handle_t {
   int firstFind;
   QString path; // Always ends with '\'
 };
-
-static int _csync_vio_local_stat_mb(const mbchar_t *uri, csync_file_stat_t *buf);
 
 csync_vio_handle_t *csync_vio_local_opendir(const QString &name) {
 
@@ -172,12 +171,9 @@ std::unique_ptr<csync_file_stat_t> csync_vio_local_readdir(csync_vio_handle_t *h
     file_stat->size = (handle->ffd.nFileSizeHigh * ((int64_t)(MAXDWORD)+1)) + handle->ffd.nFileSizeLow;
     file_stat->modtime = FileTimeToUnixTime(&handle->ffd.ftLastWriteTime, &rem);
 
-    std::wstring fullPath;
-    fullPath.reserve(handle->path.size() + std::wcslen(handle->ffd.cFileName));
-    fullPath += handle->path.toStdWString(); // path always ends with '\', by construction
-    fullPath += handle->ffd.cFileName;
+    // path always ends with '\', by construction
 
-    if (_csync_vio_local_stat_mb(fullPath.data(), file_stat.get()) < 0) {
+    if (csync_vio_local_stat(handle->path + QString::fromWCharArray(handle->ffd.cFileName), file_stat.get()) < 0) {
         // Will get excluded by _csync_detect_update.
         file_stat->type = ItemTypeSkip;
     }
@@ -185,15 +181,7 @@ std::unique_ptr<csync_file_stat_t> csync_vio_local_readdir(csync_vio_handle_t *h
     return file_stat;
 }
 
-
 int csync_vio_local_stat(const QString &uri, csync_file_stat_t *buf)
-{
-    const std::wstring wuri = uri.toStdWString();
-    int rc = _csync_vio_local_stat_mb(wuri.data(), buf);
-    return rc;
-}
-
-static int _csync_vio_local_stat_mb(const mbchar_t *wuri, csync_file_stat_t *buf)
 {
     /* Almost nothing to do since csync_vio_local_readdir already filled up most of the information
        But we still need to fetch the file ID.
@@ -204,19 +192,19 @@ static int _csync_vio_local_stat_mb(const mbchar_t *wuri, csync_file_stat_t *buf
     BY_HANDLE_FILE_INFORMATION fileInfo;
     ULARGE_INTEGER FileIndex;
 
-    h = CreateFileW( wuri, 0, FILE_SHARE_WRITE | FILE_SHARE_READ | FILE_SHARE_DELETE,
-                     NULL, OPEN_EXISTING,
-                     FILE_ATTRIBUTE_NORMAL | FILE_FLAG_BACKUP_SEMANTICS | FILE_FLAG_OPEN_REPARSE_POINT,
-                     NULL );
+    h = CreateFileW(reinterpret_cast<const wchar_t *>(OCC::FileSystem::longWinPath(uri).utf16()), 0, FILE_SHARE_WRITE | FILE_SHARE_READ | FILE_SHARE_DELETE,
+        NULL, OPEN_EXISTING,
+        FILE_ATTRIBUTE_NORMAL | FILE_FLAG_BACKUP_SEMANTICS | FILE_FLAG_OPEN_REPARSE_POINT,
+        NULL);
     if( h == INVALID_HANDLE_VALUE ) {
-        qCCritical(lcCSyncVIOLocal, "CreateFileW failed on %ls", wuri);
         errno = GetLastError();
+        qCCritical(lcCSyncVIOLocal) << "CreateFileW failed on" << uri << OCC::Utility::formatWinError(errno);
         return -1;
     }
 
     if(!GetFileInformationByHandle( h, &fileInfo ) ) {
-        qCCritical(lcCSyncVIOLocal, "GetFileInformationByHandle failed on %ls", wuri);
         errno = GetLastError();
+        qCCritical(lcCSyncVIOLocal) << "GetFileInformationByHandle failed on" << uri << OCC::Utility::formatWinError(errno);
         CloseHandle(h);
         return -1;
     }

src/libsync/filesystem.cpp
@@ -124,7 +124,7 @@ static qint64 getSizeWithCsync(const QString &filename)
     if (csync_vio_local_stat(filename, &stat) != -1) {
         result = stat.size;
     } else {
-        qCWarning(lcFileSystem) << "Could not get size for" << filename << "with csync";
+        qCWarning(lcFileSystem) << "Could not get size for" << filename << "with csync" << Utility::formatWinError(errno);
     }
     return result;
 }