stjordanis/client-1

Add socket command to upload a selection of files based on a regex

src/gui/socketapi/socketapi.h
@@ -121,6 +121,10 @@ private:
     Q_INVOKABLE void command_DELETE_ITEM(const QString &localFile, SocketListener *listener);
     Q_INVOKABLE void command_MOVE_ITEM(const QString &localFile, SocketListener *listener);
 
+    // External sync
+    Q_INVOKABLE void command_LIST_ACCOUNTS(const QString &argument, SocketListener *listener) const;
+    Q_INVOKABLE void command_UPLOAD_FILE(const QString &argument, SocketListener *listener) const;
+
     // Fetch the private link and call targetFun
     void fetchPrivateLinkUrlHelper(const QString &localFile, const std::function<void(const QString &url)> &targetFun);
 

src/gui/socketapi/socketuploadjob.h
@@ -1,0 +1,48 @@
+/*
+ * Copyright (C) by Hannah von Reth <hannah.vonreth@owncloud.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ */
+
+#pragma once
+#include <QObject>
+#include <QTemporaryFile>
+
+#include "socketapi.h"
+#include "account.h"
+
+namespace OCC {
+
+class SyncJournalDb;
+class SyncEngine;
+
+class SocketUploadJob : public QObject
+{
+    Q_OBJECT
+public:
+    SocketUploadJob(OCC::SocketListener *listener, AccountPtr acc, const QString &localDir, const QString &pattern, const QString &remoteDir, QObject *parent = nullptr);
+
+    void start();
+
+    void finish(const QString &error);
+
+    SocketListener *_listener;
+    QString _localPath;
+    QString _remotePath;
+    QString _pattern;
+    QTemporaryFile tmp;
+    SyncJournalDb *db;
+    SyncEngine *engine;
+    QStringList syncedFiles;
+
+    bool _finished = false;
+};
+}

owncloud/client

src/gui/socketapi/socketapi.h
@@ -121,6 +121,10 @@ private:
     Q_INVOKABLE void command_DELETE_ITEM(const QString &localFile, SocketListener *listener);
     Q_INVOKABLE void command_MOVE_ITEM(const QString &localFile, SocketListener *listener);
 
+    // External sync
+    Q_INVOKABLE void command_V2_LIST_ACCOUNTS(const QString &argument, SocketListener *listener) const;
+    Q_INVOKABLE void command_V2_UPLOAD_FILES_FROM(const QString &argument, SocketListener *listener) const;
+
     // Fetch the private link and call targetFun
     void fetchPrivateLinkUrlHelper(const QString &localFile, const std::function<void(const QString &url)> &targetFun);
 

src/gui/socketapi/socketuploadjob.h
@@ -1,0 +1,49 @@
+/*
+ * Copyright (C) by Hannah von Reth <hannah.vonreth@owncloud.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ */
+
+#pragma once
+#include <QObject>
+#include <QTemporaryFile>
+
+#include "socketapi.h"
+#include "account.h"
+
+namespace OCC {
+
+class SyncJournalDb;
+class SyncEngine;
+
+class SocketUploadJob : public QObject
+{
+    Q_OBJECT
+public:
+    SocketUploadJob(OCC::SocketListener *listener, const QString &argument, QObject *parent = nullptr);
+
+    void start();
+
+    void finish(const QString &error);
+
+private:
+    SocketListener *_listener;
+    QString _localPath;
+    QString _remotePath;
+    QString _pattern;
+    QTemporaryFile _tmp;
+    SyncJournalDb *_db;
+    SyncEngine *_engine;
+    QStringList _syncedFiles;
+
+    bool _finished = false;
+};
+}