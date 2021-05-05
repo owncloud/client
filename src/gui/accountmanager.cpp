nextcloud/desktop

commit 3d878bae27ba22ecdf07313725b085b84c62af1a

Fix oauth credentials

src/libsync/creds/httpcredentials.cpp
@@ -204,15 +204,14 @@ void HttpCredentials::fetchFromKeychainHelper()
     }
 
     auto readPassword = [this] {
-        auto job = _account->credentialManager()->get(passwordKeyC());
+        auto job = _account->credentialManager()->get(isUsingOAuth() ? refreshTokenKeyC() : passwordKeyC());
         connect(job, &CredentialJob::finished, this, [job, this] {
             const auto error = job->error();
             if (job->error() != error) {
                 qCWarning(lcHttpLegacyCredentials) << "Could not retrieve client password from keychain" << job->errorString();
                 return;
             }
-            bool isOauth = _account->credentialSetting(isOAuthC()).toBool();
-            if (isOauth) {
+            if (isUsingOAuth()) {
                 _refreshToken = job->data().toString();
             } else {
                 _password = job->data().toString();
@@ -347,7 +346,6 @@ void HttpCredentials::persist()
     }
 
     _account->setCredentialSetting(userC(), _user);
-    _account->setCredentialSetting(isOAuthC(), isUsingOAuth());
     if (!_clientCertBundle.isEmpty()) {
         // Note that the _clientCertBundle will often be cleared after usage,
         // it's just written if it gets passed into the constructor.

src/libsync/creds/httpcredentials_p.h
@@ -18,11 +18,6 @@ auto CredentialVersionKey()
     return QStringLiteral("CredentialVersion");
 }
 
-auto isOAuthC()
-{
-    return QStringLiteral("oauth");
-}
-
 const QString userC()
 {
     return QStringLiteral("user");
@@ -61,9 +56,10 @@ class HttpLegacyCredentials : public QObject
     {
         return QStringLiteral("_clientKeyPEM");
     }
-    const QString PasswordKey()
+
+    auto isOAuthC()
     {
-        return QStringLiteral("password");
+        return QStringLiteral("oauth");
     }
 
 public:

owncloud/client

commit 654e498ff44772d610337331aa50c978b012c69a

Fix ouath credential loading
Issue was introduced in 3d878bae27ba22ecdf07313725b085b84c62af1a

src/gui/accountmanager.cpp
@@ -311,9 +311,7 @@ AccountPtr AccountManager::loadAccountHelper(QSettings &settings)
             continue;
         acc->_settingsMap.insert(key, settings.value(key));
     }
-    DetermineAuthTypeJob::AuthType authType = settings.value(QStringLiteral("http_oauth"), false).toBool() ?
-                DetermineAuthTypeJob::AuthType::OAuth : DetermineAuthTypeJob::AuthType::Basic;
-    acc->setCredentials(new HttpCredentialsGui(authType));
+    acc->setCredentials(new HttpCredentialsGui);
 
     // now the server cert, it is in the general group
     settings.beginGroup(QLatin1String("General"));

src/gui/creds/httpcredentialsgui.h
@@ -29,11 +29,10 @@ class HttpCredentialsGui : public HttpCredentials
 {
     Q_OBJECT
 public:
-    explicit HttpCredentialsGui(DetermineAuthTypeJob::AuthType authType)
-        : HttpCredentials(authType)
+    HttpCredentialsGui()
+        : HttpCredentials()
     {
     }
-
     HttpCredentialsGui(const QString &user, const QString &password,
             const QByteArray &clientCertBundle, const QByteArray &clientCertPassword)
         : HttpCredentials(DetermineAuthTypeJob::AuthType::Basic, user, password, clientCertBundle, clientCertPassword)

src/gui/wizard/owncloudoauthcredspage.cpp
@@ -70,7 +70,7 @@ void OwncloudOAuthCredsPage::initializePage()
 {
     OwncloudWizard *ocWizard = qobject_cast<OwncloudWizard *>(wizard());
     Q_ASSERT(ocWizard);
-    ocWizard->account()->setCredentials(new HttpCredentialsGui(DetermineAuthTypeJob::AuthType::Unknown));
+    ocWizard->account()->setCredentials(new HttpCredentialsGui);
     _asyncAuth.reset(new OAuth(ocWizard->account().data(), this));
     connect(_asyncAuth.data(), &OAuth::result, this, &OwncloudOAuthCredsPage::asyncAuthResult, Qt::QueuedConnection);
     _asyncAuth->startAuthentication();

src/libsync/creds/httpcredentials.cpp
@@ -38,6 +38,11 @@ Q_LOGGING_CATEGORY(lcHttpCredentials, "sync.credentials.http", QtInfoMsg)
 Q_LOGGING_CATEGORY(lcHttpLegacyCredentials, "sync.credentials.http.legacy", QtInfoMsg)
 
 namespace {
+auto isOAuthC()
+{
+    return QStringLiteral("oauth");
+}
+
 auto passwordKeyC()
 {
     return QStringLiteral("http/password");
@@ -76,11 +81,11 @@ protected:
     {
         QNetworkRequest req(request);
         if (!req.attribute(HttpCredentials::DontAddCredentialsAttribute).toBool()) {
-            if (_cred && !_cred->password().isEmpty()) {
+            if (_cred && !_cred->_password.isEmpty()) {
                 if (_cred->isUsingOAuth()) {
-                    req.setRawHeader("Authorization", "Bearer " + _cred->password().toUtf8());
+                    req.setRawHeader("Authorization", "Bearer " + _cred->_password.toUtf8());
                 } else {
-                    QByteArray credHash = QByteArray(_cred->user().toUtf8() + ":" + _cred->password().toUtf8()).toBase64();
+                    QByteArray credHash = QByteArray(_cred->user().toUtf8() + ":" + _cred->_password.toUtf8()).toBase64();
                     req.setRawHeader("Authorization", "Basic " + credHash);
                 }
             } else if (!request.url().password().isEmpty()) {
@@ -108,12 +113,6 @@ private:
     QPointer<const HttpCredentials> _cred;
 };
 
-HttpCredentials::HttpCredentials(DetermineAuthTypeJob::AuthType authType)
-    : _authType(authType)
-{
-}
-
-// From wizard
 HttpCredentials::HttpCredentials(DetermineAuthTypeJob::AuthType authType, const QString &user, const QString &password, const QByteArray &clientCertBundle, const QByteArray &clientCertPassword)
     : _user(user)
     , _password(password)
@@ -138,16 +137,15 @@ QString HttpCredentials::user() const
     return _user;
 }
 
-QString HttpCredentials::password() const
-{
-    return _password;
-}
-
 void HttpCredentials::setAccount(Account *account)
 {
     AbstractCredentials::setAccount(account);
     if (_user.isEmpty()) {
         fetchUser();
+    }
+    const auto isOauth = account->credentialSetting(isOAuthC());
+    if (isOauth.isValid()) {
+        _authType = isOauth.toBool() ? DetermineAuthTypeJob::AuthType::OAuth : DetermineAuthTypeJob::AuthType::Basic;
     }
 }
 
@@ -345,6 +343,7 @@ void HttpCredentials::persist()
     }
     _account->setCredentialSetting(CredentialVersionKey(), CredentialVersion);
     _account->setCredentialSetting(userC(), _user);
+    _account->setCredentialSetting(isOAuthC(), isUsingOAuth());
     if (!_clientCertBundle.isEmpty()) {
         // Note that the _clientCertBundle will often be cleared after usage,
         // it's just written if it gets passed into the constructor.

src/libsync/creds/httpcredentials.h
@@ -56,7 +56,6 @@ public:
     /// Don't add credentials if this is set on a QNetworkRequest
     static constexpr QNetworkRequest::Attribute DontAddCredentialsAttribute = QNetworkRequest::User;
 
-    HttpCredentials(DetermineAuthTypeJob::AuthType authType);
     explicit HttpCredentials(DetermineAuthTypeJob::AuthType authType, const QString &user, const QString &password,
             const QByteArray &clientCertBundle = QByteArray(), const QByteArray &clientCertPassword = QByteArray());
 
@@ -67,8 +66,6 @@ public:
     bool stillValid(QNetworkReply *reply) override;
     void persist() override;
     QString user() const override;
-    // the password or token
-    QString password() const;
     void invalidateToken() override;
     void forgetSensitiveData() override;
     QString fetchUser();
@@ -85,6 +82,7 @@ public:
     // Whether we are using OAuth
     bool isUsingOAuth() const { return _authType == DetermineAuthTypeJob::AuthType::OAuth; }
 protected:
+    HttpCredentials() = default;
 
     /// Wipes legacy keychain locations
     void deleteOldKeychainEntries();