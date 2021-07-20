78bdeddd846bd6c2284fa55302c217cb9655eefb
swoichha <swoichhaa@gmail.com>
2021-07-20 15:10:57 +0545

    comment for static wait

--- test/gui/shared/steps/steps.py
+++ test/gui/shared/steps/steps.py
@@ -743,6 +743,10 @@ def step(context, resource, content):
     waitForFileToBeSynced(context, resource)
     waitForFolderToBeSynced(context, '/')
 
+    # overwriting the file immediately after it has been synced from the server seems to have some problem.
+    # The client does not see the change although the changes have already been made thus we are having a race condition
+    # So for now we add 3sec static wait
+
     snooze(3)
 
     f = open(context.userData['clientSyncPath'] + resource, "w")
