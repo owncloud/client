50fcc57e7739109c6d7075946dd31a68102c3f96
Josh Soref <jsoref@users.noreply.github.com>
2021-09-15 07:17:50 -0400

    spelling: setups
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- src/libsync/theme.h
+++ src/libsync/theme.h
@@ -422,7 +422,7 @@ public:
 
     /**
      * Whether to clear cookies before checking status.php
-     * This is used with F5 BIG-IP seups.
+     * This is used with F5 BIG-IP setups.
      */
     virtual bool connectionValidatorClearCookies() const;
 
