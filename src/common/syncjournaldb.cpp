8855f43f0f7c7113d374e8ce4ecb25885420641c
Josh Soref <jsoref@users.noreply.github.com>
2021-09-15 07:15:30 -0400

    spelling: blacklist
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- src/common/syncjournaldb.cpp
+++ src/common/syncjournaldb.cpp
@@ -826,7 +826,7 @@ bool SyncJournalDb::updateErrorBlacklistTableStructure()
     SqlQuery query(_db);
     query.prepare("CREATE INDEX IF NOT EXISTS blacklist_index ON blacklist(path collate nocase);");
     if (!query.exec()) {
-        sqlFail(QStringLiteral("updateErrorBlacklistTableStructure: create index blacklit"), query);
+        sqlFail(QStringLiteral("updateErrorBlacklistTableStructure: create index blacklist"), query);
         re = false;
     }
 
