b8d9aa98d5cb8f91e93e946e1ed55ea9a194e9fa
Erik Verbruggen <erik@verbruggen.consulting>
2021-11-26 15:49:22 +0100

    Rename a folder's navigationPaneClsid to uuid
    So we can use it not only on Windows, but also on macOS.
    
    `WinVfsSettingsVersion` has been removed, and for VFS and non-VFS cases
    the `SettingsVersion` is used.

--- src/gui/folderman.cpp
+++ src/gui/folderman.cpp
@@ -44,10 +44,7 @@ namespace {
  * [Accounts]
  * 0\version=1
  */
-auto versionC()
-{
-    return QStringLiteral("version");
-}
+const QLatin1String versionC("version");
 
 /*
  * Folders with a version > maxFoldersVersion will be removed
@@ -71,7 +68,7 @@ QString makeLegacyDbName(const OCC::FolderDefinition &def, const OCC::AccountPtr
     const QString key = QStringLiteral("%1@%2:%3").arg(account->credentials()->user(), legacyUrl.toString(), def.targetPath);
     return OCC::SyncJournalDb::makeDbName(def.localPath, QString::fromUtf8(QCryptographicHash::hash(key.toUtf8(), QCryptographicHash::Md5).left(6).toHex()));
 }
-}
+} // anonymous namespace
 
 namespace OCC {
 Q_LOGGING_CATEGORY(lcFolderMan, "gui.folder.manager", QtInfoMsg)
@@ -362,7 +359,7 @@ void FolderMan::setupFoldersHelper(QSettings &settings, AccountStatePtr account,
             Folder *f = addFolderInternal(std::move(folderDefinition), account.data(), std::move(vfs));
             if (f) {
                 // Migrate the old "usePlaceholders" setting to the root folder pin state
-                if (settings.value(versionC(), 1).toInt() == 1
+                if (settings.value(versionC, 1).toInt() == 1
                     && settings.value(QLatin1String("usePlaceholders"), false).toBool()) {
                     qCInfo(lcFolderMan) << "Migrate: From usePlaceholders to PinState::OnlineOnly";
                     f->setRootPinState(PinState::OnlineOnly);
@@ -418,12 +415,12 @@ void FolderMan::backwardMigrationSettingsKeys(QStringList *deleteKeys, QStringLi
 
     auto processSubgroup = [&](const QString &name) {
         settings->beginGroup(name);
-        const int foldersVersion = settings->value(versionC(), 1).toInt();
+        const int foldersVersion = settings->value(versionC, 1).toInt();
         if (foldersVersion <= maxFoldersVersion) {
             const auto &childGroups = settings->childGroups();
             for (const auto &folderAlias : childGroups) {
                 settings->beginGroup(folderAlias);
-                const int folderVersion = settings->value(versionC(), 1).toInt();
+                const int folderVersion = settings->value(versionC, 1).toInt();
                 if (folderVersion > FolderDefinition::maxSettingsVersion()) {
                     ignoreKeys->append(settings->group());
                 }
--- src/gui/owncloudsetupwizard.cpp
+++ src/gui/owncloudsetupwizard.cpp
@@ -405,8 +405,7 @@ void OwncloudSetupWizard::slotAssistantFinished(int result)
                 folderDefinition.virtualFilesMode = bestAvailableVfsMode();
             }
 #ifdef Q_OS_WIN
-            if (folderMan->navigationPaneHelper().showInExplorerNavigationPane())
-                folderDefinition.navigationPaneClsid = QUuid::createUuid();
+            folderDefinition.isInNavigationPane = folderMan->navigationPaneHelper().showInExplorerNavigationPane();
 #endif
 
             auto f = folderMan->addFolder(account, folderDefinition);
