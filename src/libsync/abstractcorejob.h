9c45b2f9de390f51875d46d46f3e9858cc2a4cb0
Fabian Müller <fmueller@owncloud.com>
2022-02-17 12:33:21 +0100

    Add missing docstrings

--- src/libsync/abstractcorejob.h
+++ src/libsync/abstractcorejob.h
@@ -22,6 +22,11 @@
 
 namespace OCC {
 
+/**
+ * This class manages an (HTTP) network job's result. It holds a result on success and error details on failures.
+ * The class is universally usable for all kinds of network requests, there is no difference in handling the responses.
+ * Instead, instances are created that start a suitable request and wire up the signals accordingly.
+ */
 class OWNCLOUDSYNC_EXPORT Job : public QObject
 {
     Q_OBJECT
@@ -39,11 +44,25 @@ public:
     [[nodiscard]] bool success() const;
 
 protected:
+    /**
+     * Set job result. This emits the finished() signal, and marks the job as successful.
+     * The job result or error can be set only once.
+     * @param result job result
+     */
     void setResult(const QVariant &result);
 
+    /**
+     * Set job error details. This emits the finished() signal, and marks the job as failed.
+     * The job result or error can be set only once.
+     * @param errorMessage network error or other suitable error message
+     * @param networkError network error instance or NoError if the error is not caused by a network issue
+     */
     void setError(const QString &errorMessage, QNetworkReply::NetworkError networkError);
 
 Q_SIGNALS:
+    /**
+     * Emitted when a result or error is set. May be emitted once only.
+     */
     void finished();
 
 private:
@@ -51,27 +70,54 @@ private:
     void assertNotFinished();
 
     bool _success = false;
+
     QVariant _result;
 
     QString _errorMessage;
     QNetworkReply::NetworkError _networkError = QNetworkReply::NoError;
 };
 
+
+/**
+ * Abstract base class for core job factories.
+ *
+ * Jobs are built by the startJob factory method, which creates a job instance as well as a network request, wires the required signals up, then sends the request.
+ */
 class OWNCLOUDSYNC_EXPORT AbstractCoreJobFactory : public QObject
 {
     Q_OBJECT
 
 public:
+    /**
+     * Create a new instance
+     * @param nam network access manager used to send the requests
+     * @param parent optional parent which will be set at this object's parent as well as all jobs' parent.
+     */
     explicit AbstractCoreJobFactory(QNetworkAccessManager *nam, QObject *parent = nullptr);
     ~AbstractCoreJobFactory() override;
 
+    /**
+     * Send network request and return associated job.
+     * @param url URL to send request to
+     * @return job
+     */
     virtual Job *startJob(const QUrl &url) = 0;
 
 protected:
     [[nodiscard]] QNetworkAccessManager *nam() const;
 
+    /**
+     * Set job result. Needed because the jobs' methods are protected, and this class is a friend of Job.
+     * The job result or error can be set only once.
+     * @param result job result
+     */
     static void setJobResult(Job *job, const QVariant &result);
 
+    /**
+     * Set job error details. Needed because the jobs' methods are protected, and this class is a friend of Job.
+     * @param errorMessage network error or other suitable error message
+     * @param networkError network error instance or NoError if the error is not caused by a network issue
+     */
     static void setJobError(Job *job, const QString &errorMessage, const QNetworkReply::NetworkError networkError);
 
 private:
