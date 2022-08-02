nextcloud/desktop

commit db73340fb33ecfe081ecb4764e6ff939b870e8bc

Only set the default sync root from the folder wizard if needed
Fixes: #9797

src/gui/folderwizard/folderwizard.cpp
@@ -48,7 +48,7 @@ namespace OCC {
 
 Q_LOGGING_CATEGORY(lcFolderWizard, "gui.folderwizard", QtInfoMsg)
 
-QString FolderWiardPrivate::formatWarnings(const QStringList &warnings, bool isError)
+QString FolderWizardPrivate::formatWarnings(const QStringList &warnings, bool isError)
 {
     QString ret;
     if (warnings.count() == 1) {
@@ -65,42 +65,50 @@ QString FolderWiardPrivate::formatWarnings(const QStringList &warnings, bool isE
     return ret;
 }
 
-FolderWizard::FolderWizard(AccountPtr account, QWidget *parent, Qt::WindowFlags flags)
-    : QWizard(parent, flags)
+QString FolderWizardPrivate::defaultSyncRoot() const
+{
+    if (!_account->hasDefaultSyncRoot()) {
+        return FolderMan::suggestSyncFolder(_account->url(), _account->davDisplayName());
+    } else {
+        return _account->defaultSyncRoot();
+    }
+}
+
+FolderWizardPrivate::FolderWizardPrivate(FolderWizard *q, const AccountPtr &account)
+    : q_ptr(q)
     , _account(account)
     , _folderWizardSourcePage(new FolderWizardLocalPath(account))
     , _folderWizardSelectiveSyncPage(new FolderWizardSelectiveSync(account))
 {
-    setWindowFlags(windowFlags() & ~Qt::WindowContextHelpButtonHint);
     if (account->capabilities().spacesSupport().enabled) {
-        _spacesPage = new SpacesPage(account, this);
-        setPage(Page_Space, _spacesPage);
-        _spacesPage->installEventFilter(this);
+        _spacesPage = new SpacesPage(account, q);
+        q->setPage(FolderWizard::Page_Space, _spacesPage);
+        _spacesPage->installEventFilter(q);
     }
-    setPage(Page_Source, _folderWizardSourcePage);
-    _folderWizardSourcePage->installEventFilter(this);
+    q->setPage(FolderWizard::Page_Source, _folderWizardSourcePage);
+    _folderWizardSourcePage->installEventFilter(q);
     // for now spaces are meant to be synced as a whole
     if (!account->capabilities().spacesSupport().enabled && !Theme::instance()->singleSyncFolder()) {
         _folderWizardTargetPage = new FolderWizardRemotePath(account);
-        setPage(Page_Target, _folderWizardTargetPage);
-        _folderWizardTargetPage->installEventFilter(this);
+        q->setPage(FolderWizard::Page_Target, _folderWizardTargetPage);
+        _folderWizardTargetPage->installEventFilter(q);
     }
     if (!account->capabilities().spacesSupport().enabled) {
         // TODO: add spaces support to selective sync
-        setPage(Page_SelectiveSync, _folderWizardSelectiveSyncPage);
+        q->setPage(FolderWizard::Page_SelectiveSync, _folderWizardSelectiveSyncPage);
     }
-
-    setWindowTitle(tr("Add Folder Sync Connection"));
-    setOptions(QWizard::CancelButtonOnLeft);
-    setButtonText(QWizard::FinishButton, tr("Add Sync Connection"));
-    setWizardStyle(QWizard::ModernStyle);
 }
 
-FolderWizard::~FolderWizard()
+QString FolderWizardPrivate::initialLocalPath() const
 {
+    QString defaultPath = defaultSyncRoot();
+    if (_account->capabilities().spacesSupport().enabled) {
+        defaultPath += QLatin1Char('/') + _spacesPage->selectedSpace(Spaces::SpacesModel::Columns::Name).toString();
+    };
+    return FolderMan::instance()->findGoodPathForNewSyncFolder(defaultPath);
 }
 
-QUrl FolderWizard::davUrl() const
+QUrl FolderWizardPrivate::davUrl() const
 {
     if (_account->capabilities().spacesSupport().enabled) {
         auto url = _spacesPage->selectedSpace(Spaces::SpacesModel::Columns::WebDavUrl).toUrl();
@@ -112,20 +120,7 @@ QUrl FolderWizard::davUrl() const
     return _account->davUrl();
 }
 
-QString FolderWizard::destination() const
-{
-    if (!_account->hasDefaultSyncRoot()) {
-        _account->setDefaultSyncRoot(FolderMan::suggestSyncFolder(_account->url(), _account->davDisplayName()));
-    }
-    QString defaultPath = _account->defaultSyncRoot();
-    if (_account->capabilities().spacesSupport().enabled) {
-        defaultPath += QLatin1Char('/') + _spacesPage->selectedSpace(Spaces::SpacesModel::Columns::Name).toString();
-    };
-    return FolderMan::instance()->findGoodPathForNewSyncFolder(defaultPath);
-}
-
-
-QString FolderWizard::displayName() const
+QString FolderWizardPrivate::displayName() const
 {
     if (_account->capabilities().spacesSupport().enabled) {
         return _spacesPage->selectedSpace(Spaces::SpacesModel::Columns::Name).toString();
@@ -133,20 +128,35 @@ QString FolderWizard::displayName() const
     return QString();
 }
 
-bool FolderWizard::useVirtualFiles() const
+bool FolderWizardPrivate::useVirtualFiles() const
 {
     const auto mode = bestAvailableVfsMode();
     const bool useVirtualFiles = (Theme::instance()->forceVirtualFilesOption() && mode == Vfs::WindowsCfApi) || (_folderWizardSelectiveSyncPage->useVirtualFiles());
     if (useVirtualFiles) {
-        const auto availability = Vfs::checkAvailability(destination(), mode);
+        const auto availability = Vfs::checkAvailability(initialLocalPath(), mode);
         if (!availability) {
-            auto msg = new QMessageBox(QMessageBox::Warning, tr("Virtual files are not available for the selected folder"), availability.error(), QMessageBox::Ok, ocApp()->gui()->settingsDialog());
+            auto msg = new QMessageBox(QMessageBox::Warning, FolderWizard::tr("Virtual files are not available for the selected folder"), availability.error(), QMessageBox::Ok, ocApp()->gui()->settingsDialog());
             msg->setAttribute(Qt::WA_DeleteOnClose);
             msg->open();
             return false;
         }
     }
     return useVirtualFiles;
+}
+
+FolderWizard::FolderWizard(const AccountPtr &account, QWidget *parent, Qt::WindowFlags flags)
+    : QWizard(parent, flags)
+    , d_ptr(new FolderWizardPrivate(this, account))
+{
+    setWindowFlags(windowFlags() & ~Qt::WindowContextHelpButtonHint);
+    setWindowTitle(tr("Add Folder Sync Connection"));
+    setOptions(QWizard::CancelButtonOnLeft);
+    setButtonText(QWizard::FinishButton, tr("Add Sync Connection"));
+    setWizardStyle(QWizard::ModernStyle);
+}
+
+FolderWizard::~FolderWizard()
+{
 }
 
 bool FolderWizard::eventFilter(QObject *watched, QEvent *event)
@@ -170,6 +180,26 @@ void FolderWizard::resizeEvent(QResizeEvent *event)
             setTitleFormat(titleFormat()); // And another workaround for QTBUG-3396
         }
     }
+}
+
+FolderWizard::Result FolderWizard::result()
+{
+    Q_D(FolderWizard);
+
+    const QString localPath = d->_folderWizardSourcePage->localPath();
+    if (!d->_account->hasDefaultSyncRoot()) {
+        if (FileSystem::isChildPathOf(localPath, d->defaultSyncRoot())) {
+            d->_account->setDefaultSyncRoot(d->defaultSyncRoot());
+        }
+    }
+    return {
+        d->davUrl(),
+        localPath,
+        d->_folderWizardTargetPage ? d->_folderWizardTargetPage->targetPath() : QString(),
+        d->displayName(),
+        d->useVirtualFiles(),
+        d->_folderWizardSelectiveSyncPage ? d->_folderWizardSelectiveSyncPage->selectiveSyncBlackList() : QStringList()
+    };
 }
 
 } // end namespace

src/gui/folderwizard/folderwizard_p.h
@@ -18,14 +18,38 @@
 
 #pragma once
 
+#include "folderwizard.h"
+
+#include "libsync/accountfwd.h"
+
 #include <QCoreApplication>
 #include <QStringList>
 
 namespace OCC {
 Q_DECLARE_LOGGING_CATEGORY(lcFolderWizard);
 
-namespace FolderWiardPrivate {
-    QString formatWarnings(const QStringList &warnings, bool isError = false);
-}
+class FolderWizardPrivate
+{
+public:
+    FolderWizardPrivate(FolderWizard *q, const AccountPtr &account);
+    static QString formatWarnings(const QStringList &warnings, bool isError = false);
 
+    QString initialLocalPath() const;
+
+    QString defaultSyncRoot() const;
+
+    QUrl davUrl() const;
+    bool useVirtualFiles() const;
+    QString displayName() const;
+
+private:
+    Q_DECLARE_PUBLIC(FolderWizard);
+    FolderWizard *q_ptr;
+
+    AccountPtr _account;
+    class SpacesPage *_spacesPage;
+    class FolderWizardLocalPath *_folderWizardSourcePage = nullptr;
+    class FolderWizardRemotePath *_folderWizardTargetPage = nullptr;
+    class FolderWizardSelectiveSync *_folderWizardSelectiveSyncPage = nullptr;
+};
 }

src/gui/folderwizard/folderwizardselectivesync.cpp
@@ -18,6 +18,9 @@
  */
 #include "folderwizardselectivesync.h"
 
+#include "folderwizard.h"
+#include "folderwizard_p.h"
+
 #include "gui/askexperimentalvirtualfilesfeaturemessagebox.h"
 #include "gui/selectivesyncdialog.h"
 
@@ -59,7 +62,7 @@ FolderWizardSelectiveSync::~FolderWizardSelectiveSync()
 
 void FolderWizardSelectiveSync::initializePage()
 {
-    QString targetPath = wizard()->property("targetPath").toString();
+    QString targetPath = static_cast<FolderWizard *>(wizard())->d_func()->initialLocalPath();
     QString alias = QFileInfo(targetPath).fileName();
     if (alias.isEmpty())
         alias = Theme::instance()->appName();
@@ -69,23 +72,15 @@ void FolderWizardSelectiveSync::initializePage()
 
 bool FolderWizardSelectiveSync::validatePage()
 {
-    wizard()->setProperty("selectiveSyncBlackList", useVirtualFiles() ? QVariant() : QVariant(_selectiveSync->createBlackList()));
+    if (!useVirtualFiles()) {
+        _selectiveSyncBlackList = _selectiveSync->createBlackList();
+    }
     return true;
 }
 
 bool FolderWizardSelectiveSync::useVirtualFiles() const
 {
     return _virtualFilesCheckBox && _virtualFilesCheckBox->isChecked();
-}
-
-void FolderWizardSelectiveSync::cleanupPage()
-{
-    QString targetPath = wizard()->property("targetPath").toString();
-    QString alias = QFileInfo(targetPath).fileName();
-    if (alias.isEmpty())
-        alias = Theme::instance()->appName();
-    _selectiveSync->setFolderInfo(targetPath, alias);
-    QWizardPage::cleanupPage();
 }
 
 void FolderWizardSelectiveSync::virtualFilesCheckboxClicked()
@@ -107,4 +102,9 @@ void FolderWizardSelectiveSync::virtualFilesCheckboxClicked()
             messageBox->show();
         }
     }
+}
+
+const QStringList &FolderWizardSelectiveSync::selectiveSyncBlackList() const
+{
+    return _selectiveSyncBlackList;
 }

src/gui/accountsettings.cpp
@@ -433,23 +433,15 @@ void AccountSettings::slotFolderWizardAccepted()
     FolderWizard *folderWizard = qobject_cast<FolderWizard *>(sender());
     qCInfo(lcAccountSettings) << "Folder wizard completed";
 
-    const bool useVfs = folderWizard->useVirtualFiles();
+    const auto config = folderWizard->result();
 
-    auto folder = FolderMan::instance()->addFolderFromWizard(_accountState,
-        folderWizard->field(QLatin1String("sourceFolder")).toString(),
-        folderWizard->property("targetPath").toString(),
-        folderWizard->davUrl(),
-        folderWizard->displayName(),
-        useVfs);
+    auto folder = FolderMan::instance()->addFolderFromWizardResult(_accountState, config);
 
-
-    const auto selectiveSyncBlackList = folderWizard->property("selectiveSyncBlackList").toStringList();
-    if (!selectiveSyncBlackList.isEmpty() && OC_ENSURE(folder && !useVfs)) {
-        folder->journalDb()->setSelectiveSyncList(SyncJournalDb::SelectiveSyncBlackList, selectiveSyncBlackList);
+    if (!config.selectiveSyncBlackList.isEmpty() && OC_ENSURE(folder && !config.useVirtualFiles)) {
+        folder->journalDb()->setSelectiveSyncList(SyncJournalDb::SelectiveSyncBlackList, config.selectiveSyncBlackList);
 
         // The user already accepted the selective sync dialog. everything is in the white list
-        folder->journalDb()->setSelectiveSyncList(SyncJournalDb::SelectiveSyncWhiteList,
-            QStringList() << QLatin1String("/"));
+        folder->journalDb()->setSelectiveSyncList(SyncJournalDb::SelectiveSyncWhiteList, { QLatin1String("/") });
         emit folderChanged();
     }
     FolderMan::instance()->setSyncEnabled(true);

src/gui/folderman.cpp
@@ -1456,6 +1456,11 @@ Folder *FolderMan::addFolderFromWizard(AccountStatePtr accountStatePtr, const QS
     return newFolder;
 }
 
+Folder *FolderMan::addFolderFromWizardResult(AccountStatePtr accountStatePtr, const FolderWizard::Result &config)
+{
+    return addFolderFromWizard(accountStatePtr, config.localPath, config.remotePath, config.davUrl.toString(), config.displayName, config.useVirtualFiles);
+}
+
 QString FolderMan::suggestSyncFolder(const QUrl &server, const QString &displayName)
 {
     return FolderMan::instance()->findGoodPathForNewSyncFolder(

src/gui/folderman.h
@@ -25,6 +25,8 @@
 #include "navigationpanehelper.h"
 #include "syncfileitem.h"
 
+#include "folderwizard/folderwizard.h"
+
 #include "newwizard/enums.h"
 
 class TestFolderMigration;
@@ -129,6 +131,7 @@ public:
      * In case Wizard::SyncMode::SelectiveSync is used, nullptr is returned.
      */
     Folder *addFolderFromWizard(AccountStatePtr accountStatePtr, const QString &localFolder, const QString &remotePath, const QUrl &webDavUrl, const QString &displayName, bool useVfs);
+    Folder *addFolderFromWizardResult(AccountStatePtr accountStatePtr, const FolderWizard::Result &config);
 
     /** Removes a folder */
     void removeFolder(Folder *);

src/gui/folderwizard/folderwizard.h
@@ -29,10 +29,7 @@ class Ui_FolderWizardTargetPage;
 
 namespace OCC {
 
-
-class FolderWizardLocalPath;
-class FolderWizardRemotePath;
-class FolderWizardSelectiveSync;
+class FolderWizardPrivate;
 
 /**
  * @brief The FolderWizard class
@@ -49,38 +46,47 @@ public:
         Page_SelectiveSync
     };
 
-    explicit FolderWizard(AccountPtr account, QWidget *parent = nullptr, Qt::WindowFlags flags = {});
+    struct Result
+    {
+        /***
+         * The webdav url for the sync connection.
+         */
+        QUrl davUrl;
+        /***
+         * The local folder used for the sync.
+         */
+        QString localPath;
+
+        /***
+         * The relative remote path
+         */
+        QString remotePath;
+
+        /***
+         * The Space name to display in the list of folders or an empty string.
+         */
+        QString displayName;
+
+        /***
+         * Wether to use virtual files.
+         */
+        bool useVirtualFiles;
+
+        QStringList selectiveSyncBlackList;
+    };
+
+    explicit FolderWizard(const AccountPtr &account, QWidget *parent = nullptr, Qt::WindowFlags flags = {});
     ~FolderWizard() override;
 
-    /***
-     * The webdav url for the sync connection.
-     */
-    QUrl davUrl() const;
-
-    /***
-     * The local folder used for the sync.
-     */
-    QString destination() const;
-
-    /***
-     * The Space name to display in the list of folders or an empty string.
-     */
-    QString displayName() const;
-
-    /***
-     * Wether to use virtual files.
-     */
-    bool useVirtualFiles() const;
+    Result result();
 
     bool eventFilter(QObject *watched, QEvent *event) override;
     void resizeEvent(QResizeEvent *event) override;
 
+    Q_DECLARE_PRIVATE(FolderWizard);
+
 private:
-    AccountPtr _account;
-    class SpacesPage *_spacesPage;
-    FolderWizardLocalPath *_folderWizardSourcePage = nullptr;
-    FolderWizardRemotePath *_folderWizardTargetPage = nullptr;
-    FolderWizardSelectiveSync *_folderWizardSelectiveSyncPage = nullptr;
+    QScopedPointer<FolderWizardPrivate> d_ptr;
 };
 
 

src/gui/folderwizard/folderwizardlocalpath.cpp
@@ -54,19 +54,17 @@ FolderWizardLocalPath::~FolderWizardLocalPath()
 void FolderWizardLocalPath::initializePage()
 {
     _ui->warnLabel->hide();
-    _ui->localFolderLineEdit->setText(QDir::toNativeSeparators(static_cast<FolderWizard *>(wizard())->destination()));
+    _ui->localFolderLineEdit->setText(QDir::toNativeSeparators(static_cast<FolderWizard *>(wizard())->d_func()->initialLocalPath()));
 }
 
-void FolderWizardLocalPath::cleanupPage()
+QString FolderWizardLocalPath::localPath() const
 {
-    _ui->warnLabel->hide();
+    return QDir::fromNativeSeparators(_ui->localFolderLineEdit->text());
 }
 
 bool FolderWizardLocalPath::isComplete() const
 {
-    QString errorStr = FolderMan::instance()->checkPathValidityForNewFolder(
-        QDir::fromNativeSeparators(_ui->localFolderLineEdit->text()));
-
+    QString errorStr = FolderMan::instance()->checkPathValidityForNewFolder(localPath());
 
     bool isOk = errorStr.isEmpty();
     QStringList warnStrings;
@@ -80,7 +78,7 @@ bool FolderWizardLocalPath::isComplete() const
         _ui->warnLabel->clear();
     } else {
         _ui->warnLabel->show();
-        QString warnings = FolderWiardPrivate::formatWarnings(warnStrings);
+        QString warnings = FolderWizardPrivate::formatWarnings(warnStrings);
         _ui->warnLabel->setText(warnings);
     }
     return isOk;

owncloud/client

commit 354eafb28d226f47f6412d9d99ed46caf5c86a4a

Fix regression introduced in db73340fb33ecfe081ecb4764e6ff939b870e8bc
Fixes: #9971

src/gui/folderwizard/folderwizard.cpp
@@ -108,6 +108,11 @@ QString FolderWizardPrivate::initialLocalPath() const
     return FolderMan::instance()->findGoodPathForNewSyncFolder(defaultPath);
 }
 
+QString FolderWizardPrivate::remotePath() const
+{
+    return _folderWizardTargetPage ? _folderWizardTargetPage->targetPath() : QString();
+}
+
 QUrl FolderWizardPrivate::davUrl() const
 {
     if (_account->supportsSpaces()) {
@@ -200,7 +205,7 @@ FolderWizard::Result FolderWizard::result()
     return {
         d->davUrl(),
         localPath,
-        d->_folderWizardTargetPage ? d->_folderWizardTargetPage->targetPath() : QString(),
+        d->remotePath(),
         d->displayName(),
         d->useVirtualFiles(),
         d->_folderWizardSelectiveSyncPage ? d->_folderWizardSelectiveSyncPage->selectiveSyncBlackList() : QStringList()

src/gui/folderwizard/folderwizard_p.h
@@ -36,6 +36,8 @@ public:
 
     QString initialLocalPath() const;
 
+    QString remotePath() const;
+
     QString defaultSyncRoot() const;
 
     QUrl davUrl() const;

src/gui/folderwizard/folderwizardselectivesync.cpp
@@ -62,7 +62,7 @@ FolderWizardSelectiveSync::~FolderWizardSelectiveSync()
 
 void FolderWizardSelectiveSync::initializePage()
 {
-    QString targetPath = static_cast<FolderWizard *>(wizard())->d_func()->initialLocalPath();
+    QString targetPath = static_cast<FolderWizard *>(wizard())->d_func()->remotePath();
     QString alias = QFileInfo(targetPath).fileName();
     if (alias.isEmpty())
         alias = Theme::instance()->appName();

src/gui/selectivesyncdialog.cpp
@@ -235,9 +235,7 @@ void SelectiveSyncWidget::slotUpdateDirectories(QStringList list)
     for (auto path : qAsConst(list)) {
         auto size = job ? job->sizes().value(path) : 0;
         path.remove(pathToRemove);
-        QStringList paths = path.split('/');
-        if (paths.last().isEmpty())
-            paths.removeLast();
+        const QStringList paths = path.split('/', Qt::SkipEmptyParts);
         if (paths.isEmpty())
             continue;
         if (!path.endsWith('/')) {