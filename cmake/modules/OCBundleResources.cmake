nextcloud/desktop

commit 025345351762602a435e58014650266674801ff3

Move resources to a shared libary
This allows us to use resources in the cmd app and the unit tests.

Fixes: #8839

cmake/modules/OCBundleResources.cmake
@@ -71,7 +71,7 @@ function(__write_qrc_file_footer QRC_PATH)
 endfunction()
 
 function(generate_theme TARGET OWNCLOUD_SIDEBAR_ICONS_OUT)
-    if(NOT "${OEM_THEME_DIR}" STREQUAL "${PROJECT_SOURCE_DIR}")
+    if(NOT "${OEM_THEME_DIR}" STREQUAL "${PROJECT_SOURCE_DIR}/src/resources/")
         set(QRC_PATH ${CMAKE_CURRENT_BINARY_DIR}/theme.qrc)
         __write_qrc_file_header(${QRC_PATH} theme)
 
@@ -89,6 +89,7 @@ function(generate_theme TARGET OWNCLOUD_SIDEBAR_ICONS_OUT)
         __write_qrc_file_footer(${QRC_PATH})
 
         target_sources(${TARGET} PRIVATE ${QRC_PATH})
+        target_compile_definitions(${TARGET} PRIVATE BRANDING_AVAILABLE)
 
         # add executable icon on windows and osx
         file(GLOB_RECURSE OWNCLOUD_SIDEBAR_ICONS "${OEM_THEME_DIR}/theme/colored/*-${APPLICATION_ICON_NAME}-sidebar.png")

src/resources/CMakeLists.txt
@@ -1,0 +1,37 @@
+find_package(Qt5LinguistTools)
+
+add_library(owncloudResources SHARED
+    client.qrc
+    core_theme.qrc
+    loadresources.cpp
+)
+
+generate_theme(owncloudResources OWNCLOUD_SIDEBAR_ICONS)
+
+target_link_libraries(owncloudResources PUBLIC Qt5::Core)
+
+set_target_properties(owncloudResources PROPERTIES
+    OUTPUT_NAME "${APPLICATION_EXECUTABLE}Resources"
+    AUTOUIC ON
+    AUTORCC ON
+)
+
+generate_export_header(owncloudResources
+        EXPORT_MACRO_NAME OWNCLOUDRESOURCES_EXPORT
+        EXPORT_FILE_NAME owncloudresources.h
+        STATIC_DEFINE OWNCLOUD_BUILT_AS_STATIC
+)
+
+if(Qt5LinguistTools_FOUND)
+    # Handle Translations, pick all client_* files from trans directory.
+    file(GLOB client_translations ${CMAKE_SOURCE_DIR}/translations/client_*.ts)
+    qt5_add_translation(client_compiled_translations ${client_translations})
+    target_sources(owncloudResources PRIVATE ${client_compiled_translations})
+    add_resources_to_target(
+        TARGET owncloudResources
+        PREFIX translations
+        FILES "${client_compiled_translations}"
+    )
+endif()
+
+install(TARGETS owncloudResources ${KDE_INSTALL_TARGETS_DEFAULT_ARGS})

CMakeLists.txt
@@ -39,7 +39,7 @@ include(OCBundleResources)
 if (EXISTS "${PROJECT_SOURCE_DIR}/branding")
     set(OEM_THEME_DIR "${PROJECT_SOURCE_DIR}/branding" CACHE STRING "The directory containing a custom theme")
 else()
-    set(OEM_THEME_DIR "${PROJECT_SOURCE_DIR}" CACHE STRING "Define directory containing a custom theme")
+    set(OEM_THEME_DIR "${PROJECT_SOURCE_DIR}/src/resources/" CACHE STRING "Define directory containing a custom theme")
 endif()
 include("${CMAKE_CURRENT_LIST_DIR}/THEME.cmake")
 

src/CMakeLists.txt
@@ -37,6 +37,7 @@ elseif(UNIX AND NOT APPLE)
   set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -Wl,-z,relro -Wl,-z,now")
 endif()
 
+add_subdirectory(resources)
 add_subdirectory(csync)
 add_subdirectory(libsync)
 if (NOT BUILD_LIBRARIES_ONLY)

src/gui/CMakeLists.txt
@@ -186,28 +186,8 @@ set_target_properties(owncloud PROPERTIES
     AUTOUIC ON
     AUTORCC ON
 )
-target_link_libraries(owncloud owncloudCore)
+target_link_libraries(owncloud owncloudCore owncloudResources)
 
-
-find_package(Qt5LinguistTools)
-if(Qt5LinguistTools_FOUND)
-    # Handle Translations, pick all client_* files from trans directory.
-    file(GLOB client_translations ${CMAKE_SOURCE_DIR}/translations/client_*.ts)
-    qt5_add_translation(client_compiled_translations ${client_translations})
-    target_sources(owncloud PRIVATE ${client_compiled_translations})
-    add_resources_to_target(
-        TARGET owncloud
-        PREFIX translations
-        FILES "${client_compiled_translations}"
-    )
-endif()
-
-#TODO Move resources files
-target_sources(owncloud PRIVATE
-    ${PROJECT_SOURCE_DIR}/client.qrc
-    ${PROJECT_SOURCE_DIR}/core_theme.qrc)
-
-generate_theme(owncloud OWNCLOUD_SIDEBAR_ICONS)
 MESSAGE(STATUS "OWNCLOUD_SIDEBAR_ICONS: ${APPLICATION_ICON_NAME}: ${OWNCLOUD_SIDEBAR_ICONS}")
 
 ecm_add_app_icon(appIcons ICONS "${OWNCLOUD_ICONS}" SIDEBAR_ICONS "${OWNCLOUD_SIDEBAR_ICONS}" OUTFILE_BASENAME "${APPLICATION_ICON_NAME}")

test/testutils/CMakeLists.txt
@@ -4,4 +4,4 @@ target_link_libraries(syncenginetestutils PUBLIC owncloudCore Qt5::Test)
 # testutilsloader.cpp uses Q_COREAPP_STARTUP_FUNCTION which can't used reliably in a static lib
 # therefore we compile it in the tests
 add_library(testutilsloader OBJECT testutilsloader.cpp)
-target_link_libraries(testutilsloader PUBLIC owncloudCore)
+target_link_libraries(testutilsloader PUBLIC owncloudCore owncloudResources)

owncloud/client

commit cbff86c60080cbfac6a720c0b0d5a9c8aa635552

Fix sidebar icons
Regression from: #9815

cmake/modules/OCBundleResources.cmake
@@ -98,7 +98,7 @@ function(generate_theme TARGET OWNCLOUD_SIDEBAR_ICONS_OUT)
     endif()
     if (NOT OWNCLOUD_SIDEBAR_ICONS)
         message(WARNING "The branding does not provide sidebar icons falling back to vanilla icons")
-        file(GLOB_RECURSE OWNCLOUD_SIDEBAR_ICONS "${PROJECT_SOURCE_DIR}/theme/colored/*-owncloud-icon-sidebar.png")
+        file(GLOB_RECURSE OWNCLOUD_SIDEBAR_ICONS "${PROJECT_SOURCE_DIR}/src/resources/theme/colored/*-owncloud-icon-sidebar.png")
     endif()
     set(${OWNCLOUD_SIDEBAR_ICONS_OUT} ${OWNCLOUD_SIDEBAR_ICONS} PARENT_SCOPE)
 endfunction()

src/resources/CMakeLists.txt
@@ -8,6 +8,9 @@ add_library(owncloudResources SHARED
 
 generate_theme(owncloudResources OWNCLOUD_SIDEBAR_ICONS)
 
+# make them available to the whole project
+set(OWNCLOUD_SIDEBAR_ICONS ${OWNCLOUD_SIDEBAR_ICONS} CACHE INTERNAL "Sidebar icons" FORCE)
+
 target_link_libraries(owncloudResources PUBLIC Qt5::Core)
 
 set_target_properties(owncloudResources PROPERTIES