36e5fb2ec9bc653e0800b58736c510f914fc9775
Saw-jan <saw.jan.grg3e@gmail.com>
2022-09-01 15:12:24 +0545

    remove unused step def

--- test/gui/shared/steps/steps.py
+++ test/gui/shared/steps/steps.py
@@ -423,11 +423,6 @@ def step(context, type, resource):
     waitForFileOrFolderToSync(context, resource, type)
 
 
-@Given(r'user has waited for (file|folder) to be synced', regexp=True)
-def step(context, type, resource):
-    waitForFileOrFolderToSync(context, resource, type)
-
-
 @When(r'the user waits for (file|folder) "([^"]*)" to have sync error', regexp=True)
 def step(context, type, resource):
     waitForFileOrFolderToHaveSyncError(context, resource, type)
@@ -438,7 +433,7 @@ def step(context, type, resource):
     waitForFileOrFolderToBeSyncIgnored(context, resource, type)
 
 
-@Given('the user has waited for the files to be synced')
+@Given('user has waited for the files to be synced')
 def step(context):
     waitForRootFolderToSync(context)
 
