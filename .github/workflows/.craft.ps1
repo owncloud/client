0b9596242caaba92c6721ba6666e189236a7d0b3
Hannah von Reth <hannah.vonreth@owncloud.com>
2022-09-14 11:50:39 +0200

    CI: Don't use HOME

--- .github/workflows/.craft.ps1
+++ .github/workflows/.craft.ps1
@@ -3,11 +3,11 @@ if ($IsWindows) {
 } else {
     $python = (Get-Command python3).Source
 }
-$command = @("${env:HOME}/craft/CraftMaster/CraftMaster/CraftMaster.py",
+$command = @("${env:GITHUB_WORKSPACE}/craft/CraftMaster/CraftMaster/CraftMaster.py",
              "--config", "${env:GITHUB_WORKSPACE}/.craft.ini",
              "--config-override", "${env:GITHUB_WORKSPACE}/.github/workflows/craft_override.ini",
              "--target", "${env:CRAFT_TARGET}",
-             "--variables", "WORKSPACE=${env:HOME}/craft") + $args
+             "--variables", "WORKSPACE=${env:GITHUB_WORKSPACE}/craft") + $args
 
 Write-Host "Exec: ${python} ${command}"
 
