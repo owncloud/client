7e0099adfc964da6813d11a341e270cc8360faf8
Saw-jan <saw.jan.grg3e@gmail.com>
2023-01-31 14:14:13 +0545

    extend spaces tests

--- test/gui/shared/steps/spaces_context.py
+++ test/gui/shared/steps/spaces_context.py
@@ -4,11 +4,13 @@ from helpers.UserHelper import getDisplaynameForUser, getPasswordForUser
 from helpers.SetupClientHelper import setUpClient
 from helpers.SyncHelper import waitForInitialSyncToComplete
 from helpers.SetupClientHelper import getResourcePath
+from helpers.FilesHelper import can_read, can_write, read_file_content
 from helpers.SpaceHelper import (
     create_space,
     create_space_folder,
     create_space_file,
     add_user_to_space,
+    get_file_content,
 )
 
 
@@ -42,3 +44,30 @@ def step(context, user, space_name):
     EnterPassword.loginAfterSetup(user, password)
     # wait for files to sync
     waitForInitialSyncToComplete(getResourcePath('/', user, space_name))
+
+
+@Then('user "|any|" should be able to open the file "|any|" on the file system')
+def step(context, user, file_name):
+    file_path = getResourcePath(file_name, user)
+    test.compare(can_read(file_path), True, "File should be readable")
+
+
+@Then('as "|any|" the file "|any|" should have content "|any|" on the file system')
+def step(context, user, file_name, content):
+    file_path = getResourcePath(file_name, user)
+    file_content = read_file_content(file_path)
+    test.compare(file_content, content, "Comparing file content")
+
+
+@Then('user "|any|" should not be able to edit the file "|any|" on the file system')
+def step(context, user, file_name):
+    file_path = getResourcePath(file_name, user)
+    test.compare(not can_write(file_path), True, "File should not be writable")
+
+
+@Then(
+    'as "|any|" the file "|any|" of space "|any|" should have content "|any|" in the server'
+)
+def step(context, user, file_name, space_name, content):
+    downloaded_content = get_file_content(space_name, file_name, user)
+    test.compare(downloaded_content, content, "Comparing file content")
