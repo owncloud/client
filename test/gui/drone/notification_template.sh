49090e8b44213c552ef3f16e4ad023db13d6e62c
Saw-jan <saw.jan.grg3e@gmail.com>
2023-02-15 17:28:36 +0545

    notify and upload gui reports on tag events

--- test/gui/drone/notification_template.sh
+++ test/gui/drone/notification_template.sh
@@ -18,24 +18,22 @@ for server in "${SERVERS[@]}"; do
     CURL="curl --write-out %{http_code} --silent --output /dev/null"
 
     LOGS=""
-    if [ "${DRONE_BUILD_STATUS}" == "failure" ]; then
-        GUI_LOG="${LOG_URL_PATH}/index.html"
-        SERVER_LOG="${LOG_URL_PATH}/serverlog.log"
-        STACKTRACE="${LOG_URL_PATH}/stacktrace.log"
+    GUI_LOG="${LOG_URL_PATH}/index.html"
+    SERVER_LOG="${LOG_URL_PATH}/serverlog.log"
+    STACKTRACE="${LOG_URL_PATH}/stacktrace.log"
 
-        GUI_STATUS_CODE=$($CURL "$GUI_LOG")
-        SERVER_STATUS_CODE=$($CURL "$SERVER_LOG")
-        STACKTRACE_STATUS_CODE=$($CURL "$STACKTRACE")
+    GUI_STATUS_CODE=$($CURL "$GUI_LOG")
+    SERVER_STATUS_CODE=$($CURL "$SERVER_LOG")
+    STACKTRACE_STATUS_CODE=$($CURL "$STACKTRACE")
 
-        if [[ "$GUI_STATUS_CODE" == "200" ]]; then
-            LOGS+=": [Squish Report]($GUI_LOG)"
-        fi
-        if [[ "$SERVER_STATUS_CODE" == "200" ]]; then
-            LOGS+="\n> [Server log]($SERVER_LOG)  " # 2 spaces at the end act as line-break
-        fi
-        if [[ "$STACKTRACE_STATUS_CODE" == "200" ]]; then
-            LOGS+="\n> [Stacktrace]($STACKTRACE)"
-        fi
+    if [[ "$GUI_STATUS_CODE" == "200" ]]; then
+        LOGS+=": [Squish Report]($GUI_LOG)"
+    fi
+    if [[ "$SERVER_STATUS_CODE" == "200" ]]; then
+        LOGS+="\n> [Server log]($SERVER_LOG)  " # 2 spaces at the end act as line-break
+    fi
+    if [[ "$STACKTRACE_STATUS_CODE" == "200" ]]; then
+        LOGS+="\n> [Stacktrace]($STACKTRACE)"
     fi
     if [[ -n "${LOGS}" ]]; then
         LOGS="\n${server}${LOGS}"
