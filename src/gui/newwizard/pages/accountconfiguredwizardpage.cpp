nextcloud/desktop

commit e2dd024b4e89cfc6b309585b746d6c6096795eec

Don't deselect vfs if showVirtualFilesOption (#10479)

src/gui/newwizard/pages/accountconfiguredwizardpage.cpp
@@ -44,7 +44,6 @@ AccountConfiguredWizardPage::AccountConfiguredWizardPage(const QString &defaultS
 
     if (!Theme::instance()->showVirtualFilesOption()) {
         _ui->useVfsRadioButton->setVisible(false);
-        vfsIsAvailable = false;
     }
 
     if (!vfsIsAvailable) {

owncloud/client

commit 1bf6436ff9dba6f1e41cd75c7f5b40becbc7c88b

Restore 2.11 behaviour for showVirtualFilesOption
This fixes a regression introduced in e2dd024b4e89cfc6b309585b746d6c6096795eec

src/gui/newwizard/pages/accountconfiguredwizardpage.cpp
@@ -44,6 +44,7 @@ AccountConfiguredWizardPage::AccountConfiguredWizardPage(const QString &defaultS
 
     if (!Theme::instance()->showVirtualFilesOption()) {
         _ui->useVfsRadioButton->setVisible(false);
+        enableVfsByDefault = false;
     }
 
     if (!vfsIsAvailable) {