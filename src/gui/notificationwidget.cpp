nextcloud/desktop

commit c370712d8245bda4a901f1d9ff8760a9826b4604

Replace ocsjob with JsonApiJob

src/gui/notificationwidget.cpp
@@ -20,8 +20,6 @@
 
 #include <QPushButton>
 
-#include "ocsjob.h"
-
 namespace OCC {
 
 Q_LOGGING_CATEGORY(lcNotifications, "gui.notifications", QtInfoMsg)
@@ -102,15 +100,14 @@ void NotificationWidget::slotButtonClicked(QPushButton *buttonWidget, const Acti
     }
 }
 
-void NotificationWidget::slotNotificationRequestFinished(int statusCode)
+void NotificationWidget::slotNotificationRequestFinished(bool success)
 {
     QString doneText;
     QLocale locale;
 
     QString timeStr = locale.toString(QTime::currentTime());
 
-    // the ocs API returns stat code 100 or 200 inside the xml if it succeeded.
-    if (statusCode != OCS_SUCCESS_STATUS_CODE && statusCode != OCS_SUCCESS_STATUS_CODE_V2) {
+    if (success) {
         qCWarning(lcNotifications) << "Notification Request to Server failed, leave button visible.";
         for (auto *button : qAsConst(_buttons)) {
             button->setEnabled(true);

src/gui/CMakeLists.txt
@@ -52,9 +52,7 @@ set(client_SRCS
     lockwatcher.cpp
     logbrowser.cpp
     networksettings.cpp
-    ocsjob.cpp
     ocssharejob.cpp
-    ocsshareejob.cpp
     openfilemanager.cpp
     owncloudgui.cpp
     owncloudsetupwizard.cpp

src/gui/activitywidget.cpp
@@ -35,7 +35,6 @@
 #include "notificationconfirmjob.h"
 #include "servernotificationhandler.h"
 #include "theme.h"
-#include "ocsjob.h"
 
 #include "models/activitylistmodel.h"
 #include "models/expandingheaderview.h"
@@ -338,20 +337,19 @@ void ActivityWidget::slotSendNotificationRequest(const QString &accountName, con
             connect(job, &NotificationConfirmJob::finishedSignal,
                 this, [job, this] {
                     if (job->reply()->error() == QNetworkReply::NoError) {
-                        endNotificationRequest(job->widget(), job->ocsStatus());
+                        endNotificationRequest(job->widget(), job->ocsSuccess());
                         qCInfo(lcActivity) << "Server Notification reply code" << job->ocsStatus();
 
                         // if the notification was successful start a timer that triggers
                         // removal of the done widgets in a few seconds
                         // Add 200 millisecs to the predefined value to make sure that the timer in
                         // widget's method readyToClose() has elapsed.
-                        if (job->ocsStatus() == OCS_SUCCESS_STATUS_CODE || job->ocsStatus() == OCS_SUCCESS_STATUS_CODE_V2) {
+                        if (job->ocsSuccess()) {
                             scheduleWidgetToRemove(job->widget());
                         }
                     } else {
-                        int resultCode = job->reply()->attribute(QNetworkRequest::HttpStatusCodeAttribute).toInt();
-                        endNotificationRequest(job->widget(), resultCode);
-                        qCWarning(lcActivity) << "Server notify job failed with code " << resultCode;
+                        endNotificationRequest(job->widget(), job->ocsSuccess());
+                        qCWarning(lcActivity) << "Server notify job failed with code " << job->ocsStatus();
                     }
                 });
             job->start();
@@ -365,11 +363,11 @@ void ActivityWidget::slotSendNotificationRequest(const QString &accountName, con
     }
 }
 
-void ActivityWidget::endNotificationRequest(NotificationWidget *widget, int replyCode)
+void ActivityWidget::endNotificationRequest(NotificationWidget *widget, bool success)
 {
     _notificationRequestsRunning--;
     if (widget) {
-        widget->slotNotificationRequestFinished(replyCode);
+        widget->slotNotificationRequestFinished(success);
     }
 }
 

src/gui/activitywidget.h
@@ -86,7 +86,7 @@ signals:
 private slots:
     void slotBuildNotificationDisplay(const ActivityList &list);
     void slotSendNotificationRequest(const QString &accountName, const QString &link, const QByteArray &verb);
-    void endNotificationRequest(NotificationWidget *widget, int replyCode);
+    void endNotificationRequest(NotificationWidget *widget, bool success);
     void scheduleWidgetToRemove(NotificationWidget *widget, int milliseconds = 100);
     void slotCheckToCleanWidgets();
 

src/gui/notificationwidget.h
@@ -40,7 +40,7 @@ signals:
 
 public slots:
     void setActivity(const Activity &activity);
-    void slotNotificationRequestFinished(int statusCode);
+    void slotNotificationRequestFinished(bool success);
 
 protected:
     void changeEvent(QEvent *) override;

src/gui/ocsjob.cpp
@@ -1,139 +1,0 @@
-/*
- * Copyright (C) by Roeland Jago Douma <roeland@famdouma.nl>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
- * for more details.
- */
-
-#include "ocsjob.h"
-#include "networkjobs.h"
-#include "account.h"
-
-#include <QBuffer>
-#include <QJsonDocument>
-#include <QJsonObject>
-
-namespace OCC {
-
-Q_LOGGING_CATEGORY(lcOcs, "gui.sharing.ocs", QtInfoMsg)
-
-OcsJob::OcsJob(AccountPtr account)
-    : AbstractNetworkJob(account, "")
-{
-    _passStatusCodes.append(OCS_SUCCESS_STATUS_CODE);
-    _passStatusCodes.append(OCS_SUCCESS_STATUS_CODE_V2);
-    setIgnoreCredentialFailure(true);
-}
-
-void OcsJob::setVerb(const QByteArray &verb)
-{
-    _verb = verb;
-}
-
-void OcsJob::addParam(const QString &name, const QString &value)
-{
-    _params.append(qMakePair(name, value));
-}
-
-void OcsJob::addPassStatusCode(int code)
-{
-    _passStatusCodes.append(code);
-}
-
-void OcsJob::appendPath(const QString &id)
-{
-    setPath(path() + QLatin1Char('/') + id);
-}
-
-static QUrlQuery percentEncodeQueryItems(
-    const QList<QPair<QString, QString>> &items)
-{
-    QUrlQuery result;
-    // Note: QUrlQuery::setQueryItems() does not fully percent encode
-    // the query items, see #5042
-    for (const auto &item : items) {
-        result.addQueryItem(
-            QUrl::toPercentEncoding(item.first),
-            QUrl::toPercentEncoding(item.second));
-    }
-    return result;
-}
-
-void OcsJob::start()
-{
-    // TODO: json job
-    QNetworkRequest req;
-    req.setRawHeader("Ocs-APIREQUEST", "true");
-    req.setRawHeader("Content-Type", "application/x-www-form-urlencoded");
-
-    QBuffer *buffer = new QBuffer;
-
-    QUrlQuery queryItems;
-    if (_verb == "GET") {
-        queryItems = percentEncodeQueryItems(_params);
-    } else if (_verb == "POST" || _verb == "PUT") {
-        // Url encode the _postParams and put them in a buffer.
-        QByteArray postData;
-        for (const auto &tmp : qAsConst(_params)) {
-            if (!postData.isEmpty()) {
-                postData.append("&");
-            }
-            postData.append(QUrl::toPercentEncoding(tmp.first));
-            postData.append("=");
-            postData.append(QUrl::toPercentEncoding(tmp.second));
-        }
-        buffer->setData(postData);
-    }
-    queryItems.addQueryItem(QLatin1String("format"), QLatin1String("json"));
-    QUrl url = Utility::concatUrlPath(account()->url(), path(), queryItems);
-    sendRequest(_verb, url, req, buffer);
-    AbstractNetworkJob::start();
-}
-
-bool OcsJob::finished()
-{
-    const QByteArray replyData = reply()->readAll();
-
-    QJsonParseError error;
-    auto json = QJsonDocument::fromJson(replyData, &error);
-    if (error.error != QJsonParseError::NoError) {
-        qCWarning(lcOcs) << "Could not parse reply to"
-                         << _verb
-                         << Utility::concatUrlPath(account()->url(), path())
-                         << _params
-                         << error.errorString()
-                         << ":" << replyData;
-    }
-
-    QString message;
-    const int statusCode = getJsonReturnCode(json, message);
-    if (!_passStatusCodes.contains(statusCode)) {
-        qCWarning(lcOcs) << "Reply to"
-                         << _verb
-                         << Utility::concatUrlPath(account()->url(), path())
-                         << _params
-                         << "has unexpected status code:" << statusCode << replyData;
-        emit ocsError(statusCode, message);
-    } else {
-        emit jobFinished(json);
-    }
-    return true;
-}
-
-int OcsJob::getJsonReturnCode(const QJsonDocument &json, QString &message)
-{
-    //TODO proper checking
-    auto meta = json.object().value("ocs").toObject().value("meta").toObject();
-    int code = meta.value("statuscode").toInt();
-    message = meta.value("message").toString();
-
-    return code;
-}
-}

src/gui/ocsjob.h
@@ -1,137 +1,0 @@
-/*
- * Copyright (C) by Roeland Jago Douma <roeland@famdouma.nl>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
- * for more details.
- */
-
-#ifndef OCSJOB_H
-#define OCSJOB_H
-
-#include "accountfwd.h"
-#include "abstractnetworkjob.h"
-
-#include <QVector>
-#include <QList>
-#include <QPair>
-#include <QUrl>
-
-#define OCS_SUCCESS_STATUS_CODE 100
-// Apparantly the v2.php URLs can return that
-#define OCS_SUCCESS_STATUS_CODE_V2 200
-
-class QJsonDocument;
-
-namespace OCC {
-
-/**
- * @brief The OcsShareJob class
- * @ingroup gui
- *
- * Base class for jobs that talk to the OCS endpoints on the server.
- * All the communication logic is handled in this class.
- *
- * All OCS jobs (e.g. sharing) should extend this class.
- */
-class OcsJob : public AbstractNetworkJob
-{
-    Q_OBJECT
-
-protected:
-    explicit OcsJob(AccountPtr account);
-
-    /**
-     * Set the verb for the job
-     *
-     * @param verb currently supported PUT POST DELETE
-     */
-    void setVerb(const QByteArray &verb);
-
-    /**
-     * Add a new parameter to the request.
-     * Depending on the verb this is GET or POST parameter
-     *
-     * @param name The name of the parameter
-     * @param value The value of the parameter
-     */
-    void addParam(const QString &name, const QString &value);
-
-    /**
-     * Set the post parameters
-     *
-     * @param postParams list of pairs to add (urlEncoded) to the body of the
-     * request
-     */
-    void setPostParams(const QList<QPair<QString, QString>> &postParams);
-
-    /**
-     * List of expected statuscodes for this request
-     * A warning will be printed to the debug log if a different status code is
-     * encountered
-     *
-     * @param code Accepted status code
-     */
-    void addPassStatusCode(int code);
-
-    /**
-     * The base path for an OcsJob is always the same. But it could be the case that
-     * certain operations need to append something to the URL.
-     *
-     * This function appends the common id. so <PATH>/<ID>
-     */
-    void appendPath(const QString &id);
-
-public:
-    /**
-     * Parse the response and return the status code and the message of the
-     * reply (metadata)
-     *
-     * @param json The reply from OCS
-     * @param message The message that is set in the metadata
-     * @return The statuscode of the OCS response
-     */
-    static int getJsonReturnCode(const QJsonDocument &json, QString &message);
-
-protected slots:
-
-    /**
-     * Start the OCS request
-     */
-    void start() override;
-
-signals:
-
-    /**
-     * Result of the OCS request
-     *
-     * @param reply the reply
-     */
-    void jobFinished(QJsonDocument reply);
-
-    /**
-     * The status code was not one of the expected (passing)
-     * status code for this command
-     *
-     * @param statusCode The actual status code
-     * @param message The message provided by the server
-     */
-    void ocsError(int statusCode, const QString &message);
-
-private slots:
-    bool finished() override;
-
-private:
-    QByteArray _verb;
-    QList<QPair<QString, QString>> _params;
-    QVector<int> _passStatusCodes;
-};
-}
-
-#endif // OCSJOB_H

src/gui/ocsshareejob.cpp
@@ -1,47 +1,0 @@
-/*
- * Copyright (C) by Roeland Jago Douma <roeland@owncloud.com>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
- * for more details.
- */
-
-#include "ocsshareejob.h"
-
-#include <QJsonDocument>
-
-namespace OCC {
-
-OcsShareeJob::OcsShareeJob(AccountPtr account)
-    : OcsJob(account)
-{
-    setPath("ocs/v1.php/apps/files_sharing/api/v1/sharees");
-    connect(this, &OcsJob::jobFinished, this, &OcsShareeJob::jobDone);
-}
-
-void OcsShareeJob::getSharees(const QString &search,
-    const QString &itemType,
-    int page,
-    int perPage)
-{
-    setVerb("GET");
-
-    addParam(QString::fromLatin1("search"), search);
-    addParam(QString::fromLatin1("itemType"), itemType);
-    addParam(QString::fromLatin1("page"), QString::number(page));
-    addParam(QString::fromLatin1("perPage"), QString::number(perPage));
-
-    start();
-}
-
-void OcsShareeJob::jobDone(const QJsonDocument &reply)
-{
-    emit shareeJobFinished(reply);
-}
-}

owncloud/client

commit a34aab78e6bc6bd97043b639a4e34e0d5e563350

Fix bug in handling of notifications
Introduced in https://github.com/owncloud/client/commit/c370712d8245bda4a901f1d9ff8760a9826b4604#diff-f3f71a31c7ab975532bf8db3bc6727e5b2eb2df55fe7f059be8c45bac677deefL113

src/gui/notificationwidget.cpp
@@ -117,7 +117,7 @@ void NotificationWidget::slotNotificationRequestFinished(bool success)
 
     QString timeStr = locale.toString(QTime::currentTime());
 
-    if (success) {
+    if (!success) {
         qCWarning(lcNotifications) << "Notification Request to Server failed, leave button visible.";
         for (auto *button : qAsConst(_buttons)) {
             button->setEnabled(true);