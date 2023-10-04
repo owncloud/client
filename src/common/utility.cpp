nextcloud/desktop

commit bcab27aefda27a33af7bcb70e229ecff99b95363

Fix selective sync for directories not in the sync root
The selective pattern must not start with a / as it must match the actual file name.

Fixes: #11233

src/gui/selectivesyncwidget.cpp
@@ -178,24 +178,37 @@ void SelectiveSyncWidget::slotUpdateDirectories(QStringList list)
 
     SelectiveSyncTreeViewItem *root = static_cast<SelectiveSyncTreeViewItem *>(_folderTree->topLevelItem(0));
 
-    const QString pathToRemove = Utility::concatUrlPath(davUrl(), _folderPath).path();
+    const QString rootPath = [&]() -> QString {
+        const QString path = Utility::concatUrlPath(davUrl(), _folderPath).path();
+        if (path.endsWith(QLatin1Char('/'))) {
+            return path;
+        }
+        return path + QLatin1Char('/');
+    }();
 
     // Check for excludes.
     list.erase(std::remove_if(list.begin(), list.end(),
-                   [&pathToRemove, this](const QString &it) {
-                       return _excludedFiles.isExcludedRemote(it, pathToRemove, FolderMan::instance()->ignoreHiddenFiles(), ItemTypeDirectory);
+                   [&rootPath, this](const QString &it) {
+                       return _excludedFiles.isExcludedRemote(it, rootPath, FolderMan::instance()->ignoreHiddenFiles(), ItemTypeDirectory);
                    }),
         list.end());
+
+
+    QStringList relativeList;
+    relativeList.reserve(list.size());
+    for (const QString &path : qAsConst(list)) {
+        Q_ASSERT(path.startsWith(rootPath));
+        const QString relativePath = path.mid(rootPath.size());
+        if (!relativePath.isEmpty()) {
+            relativeList.append(relativePath);
+        }
+    }
 
     // Since / cannot be in the blacklist, expand it to the actual
     // list of top-level folders as soon as possible.
     if (_oldBlackList.size() == 1 && _oldBlackList.contains(QLatin1String("/"))) {
         _oldBlackList.clear();
-        for (auto path : qAsConst(list)) {
-            path.remove(pathToRemove);
-            if (path.isEmpty()) {
-                continue;
-            }
+        for (const QString &path : qAsConst(relativeList)) {
             _oldBlackList.insert(path);
         }
     }
@@ -214,24 +227,21 @@ void SelectiveSyncWidget::slotUpdateDirectories(QStringList list)
         root->setIcon(0, Theme::instance()->applicationIcon());
         root->setData(0, Qt::UserRole, QString());
         root->setCheckState(0, Qt::Checked);
-        qint64 size = job ? job->sizes().value(pathToRemove, -1) : -1;
+        qint64 size = job ? job->sizes().value(rootPath, -1) : -1;
         if (size >= 0) {
             root->setText(1, Utility::octetsToString(size));
             root->setData(1, Qt::UserRole, size);
         }
     }
 
-    Utility::sortFilenames(list);
-    for (auto path : qAsConst(list)) {
-        auto size = job ? job->sizes().value(path) : 0;
-        path.remove(pathToRemove);
+    Utility::sortFilenames(relativeList);
+    for (const QString &path : qAsConst(relativeList)) {
+        const auto size = job ? job->sizes().value(rootPath + QLatin1Char('/') + path) : 0;
         const QStringList paths = path.split(QLatin1Char('/'), Qt::SkipEmptyParts);
-        if (paths.isEmpty())
+        if (paths.isEmpty()) {
             continue;
-        if (!path.endsWith(QLatin1Char('/'))) {
-            path.append(QLatin1Char('/'));
         }
-        recursiveInsert(root, paths, path, size);
+        recursiveInsert(root, paths, Utility::stripTrailingSlash(path), size);
     }
 
     // Root is partially checked if any children are not checked
@@ -259,9 +269,10 @@ void SelectiveSyncWidget::slotLscolFinishedWithError(QNetworkReply *r)
 void SelectiveSyncWidget::slotItemExpanded(QTreeWidgetItem *item)
 {
     QString dir = item->data(0, Qt::UserRole).toString();
-    if (dir.isEmpty())
+    if (dir.isEmpty()) {
         return;
-    PropfindJob *job = new PropfindJob(_account, davUrl(), _folderPath + dir, PropfindJob::Depth::One, this);
+    }
+    PropfindJob *job = new PropfindJob(_account, davUrl(), _folderPath + QLatin1Char('/') + dir, PropfindJob::Depth::One, this);
     job->setProperties({QByteArrayLiteral("resourcetype"), QByteArrayLiteral("http://owncloud.org/ns:size")});
     connect(job, &PropfindJob::directoryListingSubfolders, this, &SelectiveSyncWidget::slotUpdateDirectories);
     job->start();
@@ -356,11 +367,6 @@ QSet<QString> SelectiveSyncWidget::createBlackList(QTreeWidgetItem *root) const
         }
     }
     return result;
-}
-
-QSet<QString> SelectiveSyncWidget::oldBlackList() const
-{
-    return _oldBlackList;
 }
 
 qint64 SelectiveSyncWidget::estimatedSize(QTreeWidgetItem *root)

src/gui/selectivesyncwidget.h
@@ -41,11 +41,6 @@ public:
     /// Returns a list of blacklisted paths, each including the trailing /
     QSet<QString> createBlackList(QTreeWidgetItem *root = nullptr) const;
 
-    /** Returns the oldBlackList passed into setFolderInfo(), except that
-     *  a "/" entry is expanded to all top-level folder names.
-     */
-    QSet<QString> oldBlackList() const;
-
     // Estimates the total size of checked items (recursively)
     qint64 estimatedSize(QTreeWidgetItem *root = nullptr);
 

owncloud/client

commit 4f96fbca87fa25ff4ed84329a759cfae28215bd4

Properly display directory size in selective sync widget
Fixes a regression introduced in #11237.

src/common/utility.cpp
@@ -463,6 +463,16 @@ void Utility::sortFilenames(QStringList &fileNames)
     std::sort(fileNames.begin(), fileNames.end(), collator);
 }
 
+QString Utility::concatUrlPathItems(QStringList &&items, const QLatin1Char delimiter)
+{
+    for (QString &item : items) {
+        while (item.endsWith(QLatin1Char('/'))) {
+            item.chop(1);
+        }
+    }
+    return items.join(delimiter);
+}
+
 QUrl Utility::concatUrlPath(const QUrl &url, const QString &concatPath,
     const QUrlQuery &queryItems)
 {

src/common/utility.h
@@ -157,6 +157,14 @@ OCSYNC_EXPORT Q_DECLARE_LOGGING_CATEGORY(lcUtility)
         return s.toString();
     }
 
+    inline QString ensureTrailingSlash(QStringView s)
+    {
+        if (!s.endsWith(QLatin1Char('/'))) {
+            return s.toString() + QLatin1Char('/');
+        }
+        return s.toString();
+    }
+
     // Call the given command with the switch --version and rerun the first line
     // of the output.
     // If command is empty, the function calls the running application which, on
@@ -201,6 +209,11 @@ OCSYNC_EXPORT Q_DECLARE_LOGGING_CATEGORY(lcUtility)
      * @brief Sort a QStringList in a way that's appropriate for filenames
      */
     OCSYNC_EXPORT void sortFilenames(QStringList &fileNames);
+
+    /**
+     * Concatenate parts of a URL path with a given delimiter, eliminating duplicate
+     */
+    OCSYNC_EXPORT QString concatUrlPathItems(QStringList && items, const QLatin1Char delimiter = QLatin1Char('/'));
 
     /** Appends concatPath and queryItems to the url */
     OCSYNC_EXPORT QUrl concatUrlPath(

src/gui/selectivesyncwidget.cpp
@@ -114,7 +114,7 @@ void SelectiveSyncWidget::refreshFolders()
 
 void SelectiveSyncWidget::setFolderInfo(const QString &folderPath, const QString &rootName, const QSet<QString> &oldBlackList)
 {
-    _folderPath = folderPath;
+    _folderPath = Utility::stripTrailingSlash(folderPath);
     _rootName = rootName;
     _oldBlackList = oldBlackList;
     refreshFolders();
@@ -178,13 +178,7 @@ void SelectiveSyncWidget::slotUpdateDirectories(QStringList list)
 
     SelectiveSyncTreeViewItem *root = static_cast<SelectiveSyncTreeViewItem *>(_folderTree->topLevelItem(0));
 
-    const QString rootPath = [&]() -> QString {
-        const QString path = Utility::concatUrlPath(davUrl(), _folderPath).path();
-        if (path.endsWith(QLatin1Char('/'))) {
-            return path;
-        }
-        return path + QLatin1Char('/');
-    }();
+    const QString rootPath = Utility::ensureTrailingSlash(Utility::concatUrlPath(davUrl(), _folderPath).path());
 
     // Check for excludes.
     list.erase(std::remove_if(list.begin(), list.end(),
@@ -236,12 +230,12 @@ void SelectiveSyncWidget::slotUpdateDirectories(QStringList list)
 
     Utility::sortFilenames(relativeList);
     for (const QString &path : qAsConst(relativeList)) {
-        const auto size = job ? job->sizes().value(rootPath + QLatin1Char('/') + path) : 0;
+        const auto size = job ? job->sizes().value(Utility::ensureTrailingSlash(Utility::concatUrlPathItems({rootPath, path}))) : 0;
         const QStringList paths = path.split(QLatin1Char('/'), Qt::SkipEmptyParts);
         if (paths.isEmpty()) {
             continue;
         }
-        recursiveInsert(root, paths, Utility::stripTrailingSlash(path), size);
+        recursiveInsert(root, paths, Utility::ensureTrailingSlash(path), size);
     }
 
     // Root is partially checked if any children are not checked
@@ -272,7 +266,7 @@ void SelectiveSyncWidget::slotItemExpanded(QTreeWidgetItem *item)
     if (dir.isEmpty()) {
         return;
     }
-    PropfindJob *job = new PropfindJob(_account, davUrl(), _folderPath + QLatin1Char('/') + dir, PropfindJob::Depth::One, this);
+    PropfindJob *job = new PropfindJob(_account, davUrl(), Utility::concatUrlPathItems({_folderPath, dir}), PropfindJob::Depth::One, this);
     job->setProperties({QByteArrayLiteral("resourcetype"), QByteArrayLiteral("http://owncloud.org/ns:size")});
     connect(job, &PropfindJob::directoryListingSubfolders, this, &SelectiveSyncWidget::slotUpdateDirectories);
     job->start();