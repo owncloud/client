1fde700ef79469f79de0cb89855a737c4f683988
Hannah von Reth <hannah.vonreth@owncloud.com>
2024-03-26 14:03:17 +0100

    Extend accountmodalwidget to support more scenarios

--- src/gui/accountmodalwidget.cpp
+++ src/gui/accountmodalwidget.cpp
@@ -15,14 +15,53 @@
 #include "accountmodalwidget.h"
 #include "ui_accountmodalwidget.h"
 
+#include "resources/resources.h"
+
+#include <QMessageBox>
+#include <QPushButton>
+#include <QQuickWidget>
+
 namespace OCC {
-AccountModalWidget::AccountModalWidget(const QString &title, QWidget *widget, QWidget *parent)
+
+namespace {
+    auto *createQmlWidget(const QUrl &url, const QList<QQmlContext::PropertyPair> &properties)
+    {
+        auto *widget = new QQuickWidget;
+        widget->engine()->rootContext()->setContextProperties(properties);
+        widget->engine()->addImageProvider(QStringLiteral("ownCloud"), new Resources::CoreImageProvider());
+        widget->setResizeMode(QQuickWidget::SizeRootObjectToView);
+        widget->setSource(url);
+        if (!widget->errors().isEmpty()) {
+            auto box = new QMessageBox(QMessageBox::Critical, QStringLiteral("QML Error"), QDebug::toString(widget->errors()));
+            box->setAttribute(Qt::WA_DeleteOnClose);
+            box->exec();
+            qFatal("A qml error occured %s", qPrintable(QDebug::toString(widget->errors())));
+        }
+        return widget;
+    }
+}
+
+AccountModalWidget::AccountModalWidget(
+    const QString &title, const QUrl &qmlSrc, const QList<QQmlContext::PropertyPair> &properties, const QList<Button> &buttons, QWidget *parent)
+    : AccountModalWidget(title, createQmlWidget(qmlSrc, properties), buttons, parent)
+{
+}
+
+
+AccountModalWidget::AccountModalWidget(const QString &title, QWidget *widget, const QList<Button> &buttons, QWidget *parent)
     : QWidget(parent)
     , ui(new Ui::AccountModalWidget)
 {
     ui->setupUi(this);
     ui->groupBox->setTitle(title);
     ui->groupBox->layout()->addWidget(widget);
+
+    if (!buttons.isEmpty()) {
+        ui->buttonBox->clear();
+        for (auto [button, role] : buttons) {
+            ui->buttonBox->addButton(button, role);
+        }
+    }
 
     connect(ui->buttonBox, &QDialogButtonBox::accepted, this, [this] {
         Q_EMIT accepted();
@@ -32,5 +71,18 @@ AccountModalWidget::AccountModalWidget(const QString &title, QWidget *widget, QW
         Q_EMIT rejected();
         Q_EMIT finished();
     });
+
+    connect(ui->buttonBox, &QDialogButtonBox::clicked, this, [this](auto *button) { _clickedButton = button; });
 }
+
+QDialogButtonBox *AccountModalWidget::buttons()
+{
+    return ui->buttonBox;
+}
+
+QAbstractButton *AccountModalWidget::clickedButton() const
+{
+    return _clickedButton;
+}
+
 } // OCC
