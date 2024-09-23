b53efea60131a0d171f60f0099c8df3df7daa835
Erik Verbruggen <erik@verbruggen.consulting>
2024-09-23 17:31:17 +0200

    Fix count of synced spaces
    When a new account is added, the settings page for the account is
    created with the account in the disconnected state. At that point, it is
    not yet known if the server supports spaces, so there is no space
    manager yet, and the update signal is not connected. Moving the signal
    connect to `slotAccountStateChanged` for the `Connected` state fixes the
    issue.
    
    Fixes: #11889

--- src/gui/accountsettings.cpp
+++ src/gui/accountsettings.cpp
@@ -488,6 +488,10 @@ void AccountSettings::slotAccountStateChanged()
             icon = StatusIcon::Warning;
         }
         showConnectionLabel(tr("Connected"), icon, errors);
+        if (accountsState()->supportsSpaces()) {
+            connect(accountsState()->account()->spacesManager(), &GraphApi::SpacesManager::updated, this, &AccountSettings::slotSpacesUpdated);
+            slotSpacesUpdated();
+        }
         break;
     }
     case AccountState::ServiceUnavailable:
