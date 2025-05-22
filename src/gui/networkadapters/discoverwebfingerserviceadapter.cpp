898174777bf2e366d6fe0f36d7124d5cc1f34c05
modSpike <lisa.reese@kiteworks.com>
2025-05-22 10:12:15 +0200

    fixed copyrights

--- src/gui/networkadapters/discoverwebfingerserviceadapter.cpp
+++ src/gui/networkadapters/discoverwebfingerserviceadapter.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) Fabian Müller <fmueller@owncloud.com>
+ * Copyright (C) Lisa Reese <lisa.reese@kiteworks.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
