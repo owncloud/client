8dba5e70e1ad5a1da67037e764d2022bd6f040aa
Prajwol Amatya <prajwolamatya11@gmail.com>
2025-05-27 17:17:36 +0545

    filter out STATUS:NOP during sync status check

--- test/gui/shared/scripts/helpers/SyncHelper.py
+++ test/gui/shared/scripts/helpers/SyncHelper.py
@@ -87,8 +87,8 @@ SYNC_PATTERNS = {
     'error': [SYNC_STATUS['ERROR']],
 }
 
+# checking initial sync of Personal sync shows these sync status
 if is_windows():
-    SYNC_PATTERNS['initial'].append([SYNC_STATUS['REGISTER'], SYNC_STATUS['UPDATE']])
     SYNC_PATTERNS['initial'].append([SYNC_STATUS['UPDATE'], SYNC_STATUS['UPDATE']])
 
 
@@ -189,7 +189,7 @@ def filter_messages_for_item(messages, item):
     for msg in messages:
         msg = msg.rstrip('/').rstrip('\\')
         item = item.rstrip('/').rstrip('\\')
-        if msg.endswith(item):
+        if msg.endswith(item) and not msg.startswith(SYNC_STATUS['NOP']):
             filtered_messages.append(msg)
     return filtered_messages
 
