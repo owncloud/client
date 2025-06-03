22a6be9b7d4b94301e48fa23f9dcf8d70e5ca97a
Prajwol Amatya <prajwolamatya11@gmail.com>
2025-06-03 14:09:35 +0545

    automate branded client

--- test/gui/shared/scripts/helpers/ConfigHelper.py
+++ test/gui/shared/scripts/helpers/ConfigHelper.py
@@ -74,6 +74,7 @@ CONFIG_ENV_MAP = {
     'tempFolderPath': 'TEMP_FOLDER_PATH',
     'clientConfigDir': 'CLIENT_CONFIG_DIR',
     'guiTestReportDir': 'GUI_TEST_REPORT_DIR',
+    'predefined_users': 'PREDEFINED_USERS',
     'ocis': 'OCIS',
     'record_video_on_failure': 'RECORD_VIDEO_ON_FAILURE',
 }
@@ -97,6 +98,7 @@ CONFIG = {
     'tempFolderPath': os.path.join(get_client_root_path(), 'temp'),
     'clientConfigDir': get_config_home(),
     'guiTestReportDir': os.path.abspath('../reports'),
+    'predefined_users': False,
     'ocis': False,
     'record_video_on_failure': False,
     'files_for_upload': os.path.join(CURRENT_DIR.parent.parent, 'files-for-upload'),
