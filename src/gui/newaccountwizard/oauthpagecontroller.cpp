142bc581ba96b88ae2ae558f0c23dd98d2fd6b86
modSpike <lisa.reese@kiteworks.com>
2025-06-26 13:19:22 +0200

    laid out the widgets in the new account wizard page 2 also implemented a function to create the copy icon using the current wizard's text color as this is the only way I could imagine to be sure it will always be visible against the theme's background color

--- src/gui/newaccountwizard/oauthpagecontroller.cpp
+++ src/gui/newaccountwizard/oauthpagecontroller.cpp
@@ -13,7 +13,19 @@
  */
 #include "oauthpagecontroller.h"
 
+#include "resources.h"
+#include "template.h"
+#include "theme.h"
+
+#include <QHBoxLayout>
+#include <QLabel>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QSignalBlocker>
+#include <QVBoxLayout>
 #include <QWizardPage>
+#include <qbuffer.h>
+#include <qimagereader.h>
 
 namespace OCC {
 
@@ -29,8 +41,87 @@ void OAuthPageController::buildPage()
     if (!_page)
         return;
 
-    _page->setTitle(QStringLiteral("OAuthPage"));
+    QString appName = Theme::instance()->appNameGUI();
+
+    QLabel *logoLabel = new QLabel({}, _page);
+    logoLabel->setPixmap(Theme::instance()->wizardHeaderLogo().pixmap(200, 100));
+    logoLabel->setAlignment(Qt::AlignCenter);
+    logoLabel->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Fixed);
+    logoLabel->setAccessibleName(tr("Application Name Logo %1").arg(appName));
+
+    QLabel *instructionLabel =
+        new QLabel(tr("Leave this screen open. A sign in prompt will appear in your web browser to connect you to the following address."), _page);
+    QFont font = instructionLabel->font();
+    font.setPixelSize(14);
+    instructionLabel->setFont(font);
+    instructionLabel->setWordWrap(true);
+    instructionLabel->setAlignment(Qt::AlignCenter);
+    instructionLabel->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Fixed);
+
+    _urlField = new QLineEdit(_page);
+    _urlField->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Fixed);
+    _urlField->setEnabled(false);
+    _urlField->setAccessibleDescription(tr("Login URL"));
+
+    //    QIcon copyIcon = copyIcon();
+    QPushButton *copyButton = new QPushButton(copyIcon(), QString(), _page);
+    copyButton->setFlat(true);
+    copyButton->setContentsMargins(0, 0, 0, 0);
+    copyButton->setFixedSize(_urlField->height(), _urlField->height());
+    copyButton->setAccessibleDescription(tr("Copy the login URL to the clipboard"));
+    connect(copyButton, &QPushButton::clicked, this, &OAuthPageController::copyUrlClicked);
+
+    QLabel *footerLogoLabel = nullptr;
+    if (!Theme::instance()->wizardFooterLogo().isNull()) {
+        footerLogoLabel = new QLabel({}, _page);
+        footerLogoLabel->setPixmap(Theme::instance()->wizardFooterLogo().pixmap(100, 52));
+        footerLogoLabel->setAlignment(Qt::AlignCenter);
+        footerLogoLabel->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Fixed);
+        // this is not the same logo as the header logo but we have no idea what this logo could be.
+        // this is the designer's suggestion so far
+        footerLogoLabel->setAccessibleName(tr("Additional logo defined by the organization"));
+    }
+
+    QVBoxLayout *layout = new QVBoxLayout();
+    layout->setContentsMargins(50, 0, 50, 0);
+    layout->setSpacing(12);
+    layout->addStretch(1);
+    layout->addWidget(logoLabel, Qt::AlignCenter);
+    layout->addSpacing(16);
+    layout->addWidget(instructionLabel, Qt::AlignCenter);
+
+    QHBoxLayout *urlAreaLayout = new QHBoxLayout();
+    urlAreaLayout->setContentsMargins(0, 0, 0, 0);
+    urlAreaLayout->setSpacing(0);
+    urlAreaLayout->addWidget(_urlField, Qt::AlignLeft);
+    urlAreaLayout->addWidget(copyButton);
+    layout->addLayout(urlAreaLayout, Qt::AlignCenter);
+    if (footerLogoLabel)
+        layout->addWidget(footerLogoLabel, Qt::AlignCenter);
+    layout->addStretch(1);
+    _page->setLayout(layout);
 }
+
+QIcon OAuthPageController::copyIcon()
+{
+    // this is required because the background color of the wizard pages can be set via theming and we can't pick an icon
+    // color that "fits" based on the current
+    // normally we would want to cache this but for this very special one off case I don't think it makes sense
+    const QString iconPath = QStringLiteral(":/client/resources/core/copy.svg");
+    QPalette pagePalette = _page->palette();
+    const QString color = pagePalette.color(QPalette::Text).name();
+    QByteArray data = Resources::Template::renderTemplateFromFile(iconPath, {{QStringLiteral("color"), color}}).toUtf8();
+    QBuffer buffer(&data);
+    QImageReader iconReader(&buffer, "svg");
+    return QIcon(QPixmap::fromImageReader(&iconReader));
+}
+
+void OAuthPageController::setUrl(const QUrl &url)
+{
+    _urlField->setText(url.toDisplayString());
+}
+
+void OAuthPageController::copyUrlClicked() { }
 
 bool OAuthPageController::validate()
 {
