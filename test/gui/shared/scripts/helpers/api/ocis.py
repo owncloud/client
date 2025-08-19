f22f09bdb06b50761391024e559763a0cbfc7d56
Prajwol Amatya <prajwolamatya11@gmail.com>
2025-08-19 13:59:10 +0545

    delete oc10 related test codes

--- test/gui/shared/scripts/helpers/api/ocis.py
+++ test/gui/shared/scripts/helpers/api/ocis.py
@@ -9,50 +9,12 @@ def get_graph_url():
     return url_join(get_config("localBackendUrl"), "graph", "v1.0")
 
 
-def create_group(group_name):
-    url = url_join(get_graph_url(), "groups")
-    body = json.dumps({"displayName": group_name})
-    response = request.post(url, body)
-    request.assert_http_status(response, 201, f"Failed to create group '{group_name}'")
-    resp_object = response.json()
-    return {
-        "id": resp_object["id"],
-        "displayName": resp_object["displayName"],
-    }
-
-
-def delete_group(group_id):
-    url = url_join(get_graph_url(), "groups", group_id)
-    response = request.delete(url)
-    request.assert_http_status(response, 200, f"Failed to delete group '{group_id}'")
-
-
-def get_group_id(group_name):
-    url = url_join(get_graph_url(), "groups", group_name)
-    response = request.get(url)
-    request.assert_http_status(response, 200, f"Failed to get group '{group_name}'")
-    resp_object = response.json()
-    return resp_object["id"]
-
-
 def get_user_id(user):
     url = url_join(get_graph_url(), "users", user)
     response = request.get(url)
     request.assert_http_status(response, 200, f"Failed to get user '{user}'")
     resp_object = response.json()
     return resp_object["id"]
-
-
-def add_user_to_group(user, group_name):
-    url = url_join(
-        get_graph_url(), "groups", get_group_id(group_name), "members", "$ref"
-    )
-    data = url_join(get_graph_url(), "users", get_user_id(user))
-    body = json.dumps({"@odata.id": data})
-    response = request.post(url, body)
-    request.assert_http_status(
-        response, 204, f"Failed to add user '{user}' to group '{group_name}'"
-    )
 
 
 def create_user(username, password, displayname, email):
--- test/gui/shared/scripts/helpers/api/provisioning.py
+++ test/gui/shared/scripts/helpers/api/provisioning.py
@@ -1,34 +1,8 @@
-from helpers.api import oc10 as oc
 from helpers.api import ocis
 from helpers.ConfigHelper import get_config
 from helpers import UserHelper
 
-created_groups = {}
 created_users = {}
-
-
-def create_group(group_name):
-    if get_config('ocis'):
-        group_info = ocis.create_group(group_name)
-    else:
-        group_info = oc.create_group(group_name)
-    created_groups[group_name] = group_info
-
-
-def delete_created_groups():
-    for group_name, group_info in list(created_groups.items()):
-        if get_config('ocis'):
-            ocis.delete_group(group_info['id'])
-        else:
-            oc.delete_group(group_info['id'])
-        del created_groups[group_name]
-
-
-def add_user_to_group(user, group_name):
-    if get_config('ocis'):
-        ocis.add_user_to_group(user, group_name)
-    else:
-        oc.add_user_to_group(user, group_name)
 
 
 def create_user(username):
@@ -52,28 +26,17 @@ def create_user(username):
             password=UserHelper.get_default_password(),
         )
 
-    if get_config('ocis'):
-        user_info = ocis.create_user(
-            user.username,
-            user.password,
-            user.displayname,
-            user.email,
-        )
-    else:
-        user_info = oc.create_user(
-            user.username,
-            user.password,
-            user.displayname,
-            user.email,
-        )
+    user_info = ocis.create_user(
+        user.username,
+        user.password,
+        user.displayname,
+        user.email,
+    )
     created_users[username] = user_info
 
 
 def delete_created_users():
     for username, user_info in list(created_users.items()):
         if get_config('client_name') == 'ownCloud':
-            if get_config('ocis'):
-                ocis.delete_user(user_info['id'])
-            else:
-                oc.delete_user(user_info['id'])
+            ocis.delete_user(user_info['id'])
         del created_users[username]
