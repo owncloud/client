67bbb0cbbad14889d3b45e37b067460033a14dc0
Erik Verbruggen <erik@verbruggen.consulting>
2025-08-25 17:14:18 +0200

    Remove legacy settings loading
    This code was used to load pre-2.4 settings. Latest substantial changes
    were from 2018.

--- src/gui/accountmanager.cpp
+++ src/gui/accountmanager.cpp
@@ -108,11 +108,6 @@ bool AccountManager::restore()
 
     // If there are no accounts, check the old format.
     const auto &childGroups = settings->childGroups();
-    if (childGroups.isEmpty()) {
-        restoreFromLegacySettings();
-        return true;
-    }
-
     for (const auto &accountId : childGroups) {
         settings->beginGroup(accountId);
         if (auto acc = loadAccountHelper(*settings)) {
@@ -125,71 +120,6 @@ bool AccountManager::restore()
     }
 
     return true;
-}
-
-bool AccountManager::restoreFromLegacySettings()
-{
-    static const auto logPrefix = QStringLiteral("Legacy settings migration: ");
-
-    qCInfo(lcAccountManager) << logPrefix
-                             << "restoreFromLegacySettings, checking settings group"
-                             << Theme::instance()->appName();
-
-    // try to open the correctly themed settings
-    auto settings = ConfigFile::settingsWithGroup(Theme::instance()->appName());
-
-    // if the settings file could not be opened, the childKeys list is empty
-    // then try to load settings from a very old place
-    if (settings->childKeys().isEmpty()) {
-        // Now try to open the original ownCloud settings to see if they exist.
-        QString oCCfgFile = QDir::fromNativeSeparators(settings->fileName());
-        // replace the last two segments with ownCloud/owncloud.cfg
-        oCCfgFile = oCCfgFile.left(oCCfgFile.lastIndexOf(QLatin1Char('/')));
-        oCCfgFile = oCCfgFile.left(oCCfgFile.lastIndexOf(QLatin1Char('/')));
-        oCCfgFile += QLatin1String("/ownCloud/owncloud.cfg");
-
-        qCInfo(lcAccountManager) << logPrefix
-                                 << "checking old config " << oCCfgFile;
-
-#ifdef Q_OS_WIN
-        Utility::NtfsPermissionLookupRAII ntfs_perm;
-#endif
-        QFileInfo fi(oCCfgFile);
-        if (fi.isReadable()) {
-            auto oCSettings = std::make_unique<QSettings>(oCCfgFile, QSettings::IniFormat);
-            oCSettings->beginGroup(QStringLiteral("ownCloud"));
-
-            // Check the theme URL to see if it is the same URL that the oC config was for
-            QString overrideUrl = Theme::instance()->overrideServerUrlV2();
-            if (!overrideUrl.isEmpty()) {
-                if (overrideUrl.endsWith(QLatin1Char('/'))) {
-                    overrideUrl.chop(1);
-                }
-                QString oCUrl = oCSettings->value(urlC()).toString();
-                if (oCUrl.endsWith(QLatin1Char('/'))) {
-                    oCUrl.chop(1);
-                }
-
-                // in case the urls are equal reset the settings object to read from
-                // the ownCloud settings object
-                qCInfo(lcAccountManager) << logPrefix
-                                         << "Migrate oC config if " << oCUrl << " == " << overrideUrl << ":"
-                                         << (oCUrl == overrideUrl ? "Yes" : "No");
-                if (oCUrl == overrideUrl) {
-                    settings = std::move(oCSettings);
-                }
-            }
-        }
-    }
-
-    // Try to load the single account.
-    if (!settings->childKeys().isEmpty()) {
-        if (auto acc = loadAccountHelper(*settings)) {
-            addAccount(acc);
-            return true;
-        }
-    }
-    return false;
 }
 
 AccountPtr AccountManager::createAccount(const NewAccountModel &model)
