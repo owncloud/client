ed8553c88ea478edf8dbfae96ffe51feba1b39fb
Thomas Müller <1005065+DeepDiver1975@users.noreply.github.com>
2025-09-29 10:15:42 +0200

    fix: application crash when the socket is deleted before the read opation finished

--- src/gui/socketapi/socketapi.cpp
+++ src/gui/socketapi/socketapi.cpp
@@ -234,9 +234,15 @@ void SocketApi::slotReadSocket()
     // a SocketListener that doesn't send any messages.
     static auto invalidListener = QSharedPointer<SocketListener>::create(nullptr);
     const auto listener = _listeners.value(socket, invalidListener);
-    while (socket->canReadLine()) {
-        // Make sure to normalize the input from the socket to
-        // make sure that the path will match, especially on OS X.
+    while (true) {
+        // listener->socket is a QPointer and will be null as soon as the socket is deleted in the read loop
+        auto listenerSocket = listener->socket;
+        if (listenerSocket.isNull()) {
+            break;
+        }
+        if (!socket->canReadLine()) {
+            break;
+        }
         QString line = QString::fromUtf8(socket->readLine()).normalized(QString::NormalizationForm_C);
         // Note: do NOT use QString::trimmed() here! That will also remove any trailing spaces (which _are_ part of the filename)!
         line.chop(1); // remove the '\n'
